2077c6e4b601b267b851263d7424f90fad6abc9d
assafvayner <assafvayner@gmail.com>
2019-10-22 19:39:06 -0700

    distance method in Geometry

---
+++ src/Pathfinding/Geometry.cpp
@@ -0,0 +1,6 @@
+#include "Geometry.h"
+#include <cmath>
+
+double distance(Point a, Point b){
+    return(sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y) *  (a.y - b.y)));
+}
--- src/Pathfinding/Geometry.h
+++ src/Pathfinding/Geometry.h
@@ -1,15 +1,19 @@
 #pragma once
 
 struct Node{
-    Point coordinate;
+    Point location;//current location of node relative to origin
     Node& north;
     Node& south;
     Node& east;
     Node& west;
-    bool blocked;
+    bool isBlocked;//False is no obstacle, True is obstacle
+
+    //when creating node, give null values to unknown parts
 };
 
 struct Point{
     double x; //float?
     double y; //float?
 };
+
+double distance(Point a, Point b);
