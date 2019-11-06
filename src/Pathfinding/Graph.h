243fef32af5b5146ce40ba9e520e333e1f9b94fb
shahm5 <shahm5@DESKTOP-V5DTHPI.localdomain>
2019-11-05 19:57:49 -0800

    began creating graph

---
+++ src/Pathfinding/Graph.h
@@ -0,0 +1,38 @@
+#pragma once
+#include<vector>
+#include "Geometry.h"
+using node_ptr = std::shared_ptr<Node>;
+
+struct Neighbor {
+    // x and y are -1 if the point doesn't exist
+    int xIndex;
+    int yIndex;
+    int distance;
+}
+
+struct Node {
+    // int currX;
+    // int currY;
+    Neighbor forwards;
+    Neighbor backwards;
+    Neighbor left;
+    Neighbor right;
+    int distance;
+}
+
+class Graph
+{
+    public:
+
+
+        // int radius = 20;
+        // int step = 1;
+        // std::vector<Node> map;
+        // Node robot_current_location;
+        // void updateRobotLocation(Node newPoint);
+        // void createNode(Point p);
+        // double distanceBetweenNodes(node_ptr a, node_ptr b);
+
+        // Node getNode(Point p);
+        // Map();
+};
