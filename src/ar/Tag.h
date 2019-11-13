bd98a9b4275a8da59881dbf93e9e20cc0199763b
Evan Straw <evan.straw99@gmail.com>
2019-11-12 17:31:09 -0800

    Add documentation.

--- src/ar/Tag.h
+++ src/ar/Tag.h
@@ -48,43 +48,84 @@ namespace AR
 #endif
 
 	// Matrices constructed from Camera and Distortion Parameters
+	/** OpenCV matrix containing the intrinsic camera parameters. */
 	const cv::Mat CAMERA_PARAMS(3,3,CV_64FC1,*CAMERA_PARAMS_ARRAY);
+	/** OpenCV matrix containing the distortion parameters. */
 	const cv::Mat DISTORTION_PARAMS(5,1,CV_64FC1,*DISTORTION_PARAMS_ARRAY);
-	
+
+	/**
+	   Struct type representing a corner of a tag.
+	   Includes internal angle and the coordinates as an OpenCV Point.
+	 */
 	struct Corner
 	{
 		double angle;
 		cv::Point point;
 	};
 
+	/**
+	   Exception class thrown in the event of invalid corner ordering. 
+	   @see Tag()
+	 */
 	class InvalidCornerException: public std::exception
 	{
 		virtual const char* what() const throw();
 	};
 
+	/**
+	   @brief Enum containing values for the corner indices.
+
+	   This is mostly used internally but may be used as a reference to see what the correct
+	   corner ordering is. The order currently goes clockwise starting with top left.
+	 */
 	enum CornerIndex{
 		TOP_LEFT = 0,
 		TOP_RIGHT= 1,
 		BOTTOM_RIGHT = 2,
 		BOTTOM_LEFT = 3
 	};
-	
+
+	/**
+	   @brief Class representing an AR tag detected in an image.
+
+	   This class is simply used as a way to represent the result of a detection, and can
+	   provide location and orientation information. You should likely never need to construct
+	   a Tag yourself; this is done internally by the detection code. All tags are immutable
+	   after construction.
+	 */
 	class Tag
 	{
 	private:
+		/** Stores the corners of the tag in the image */
 		std::vector<Corner> corners;
+		/** Stores the orientation Euler angles */
 		cv::Vec3d orientation;
-		cv::Vec3d calcOrientation();
+		/** Calculates the orientation Euler angles */
+		cv::Vec3d calcOrientation(); 
 	public:
+		/**
+		   Constructor for a Tag. Takes four OpenCV points which are the coordinates of the
+		   corners of the Tag in the image. Note the order of points; this will be checked and
+		   an AR::InvalidCornerException will be thrown if ordering is incorrect.
+		 */
 		Tag(cv::Point top_left,
 			cv::Point top_right,
 			cv::Point bottom_right,
 			cv::Point bottom_left);
+		/** Gets the coordinates of the center of the tag. */
 		cv::Point getCenter() const;
+		/** 
+			Gets the corners of the tag. Note that this is a const operation and a copy of the
+			corners will be returned.
+		*/
 		std::vector<Corner> getCorners() const;
+		/** Gets the pitch (rotation about x axis) of the tag */
 		float getPitch() const;
+		/** Gets the yaw (rotation about y axis) of the tag */
 		float getYaw() const;
+		/** Gets the roll (rotation about z axis) of the tag */
 		float getRoll() const;
-		float getDistance();
+		/** Gets the distance from the camera to the tag. CURRENTLY UNIMPLEMENTED. */
+		float getDistance(); 
 	};
 }
