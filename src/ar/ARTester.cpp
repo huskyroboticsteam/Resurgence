f83c020a88e12effa32eb88a8cef8fd96a235d1d
Evan Straw <evan.straw99@gmail.com>
2019-11-12 18:07:54 -0800

    Fix some issues with tabs and spaces

--- src/ar/ARTester.cpp
+++ src/ar/ARTester.cpp
@@ -24,71 +24,71 @@ int main()
 
     int api_id = cv::CAP_ANY;
 
-	int thresh_val = 128;
-	int thresh2_val = 128;
-	int blur_val = 2;
+    int thresh_val = 128;
+    int thresh2_val = 128;
+    int blur_val = 2;
 
     cout << "Opening camera..." << endl;
 
-	cap.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
-	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
-	
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+
     cap.open(CAMERA_ID + api_id);
     if(!cap.isOpened())
-	{
-		cerr << "ERROR! Unable to open camera" << endl;
+    {
+        cerr << "ERROR! Unable to open camera" << endl;
         return 1;
-	}
+    }
 
-	cout << "Opening image window, press Q to quit" << endl;
+    cout << "Opening image window, press Q to quit" << endl;
 
-	cv::namedWindow(ORIG_WINDOW_NAME);
-	cv::createTrackbar(THRESH_TRACKBAR_NAME, ORIG_WINDOW_NAME, &thresh_val, 256);
-	cv::createTrackbar(THRESH2_TRACKBAR_NAME, ORIG_WINDOW_NAME, &thresh2_val, 256);
-	cv::createTrackbar(BLUR_TRACKBAR_NAME, ORIG_WINDOW_NAME, &blur_val, 10);
-	
+    cv::namedWindow(ORIG_WINDOW_NAME);
+    cv::createTrackbar(THRESH_TRACKBAR_NAME, ORIG_WINDOW_NAME, &thresh_val, 256);
+    cv::createTrackbar(THRESH2_TRACKBAR_NAME, ORIG_WINDOW_NAME, &thresh2_val, 256);
+    cv::createTrackbar(BLUR_TRACKBAR_NAME, ORIG_WINDOW_NAME, &blur_val, 10);
+
     while(true)
-	{
-		cap.read(frame);
-		if(frame.empty())
-		{
-			cerr << "ERROR! Blank frame grabbed" << endl;
-			break;
-		}
-
-		// Mats to hold grayscale and edge detection results
-		cv::Mat gray;
-		cv::Mat edges;
-
-		// pass frame to detector here
-		std::vector<AR::Tag> tags = AR::findTags(frame, gray, edges,
-												 thresh_val, thresh2_val, blur_val);
-
-		// show locations of tags in window
-		std::cout << "Found " << tags.size() << " tags" << std::endl;
-		
-		// draw lines between tag points
-		for(AR::Tag tag : tags)
-		{
-			std::vector<AR::Corner> corners = tag.getCorners();
-			corners.push_back(corners[0]);
-			for(int i = 0; i < corners.size()-1; i++)
-			{
-				cv::line(frame, corners[i].point, corners[i + 1].point, cv::Scalar(0, 0, 255), 3);
-			}
-			std::cout << tag.getCenter() << ", ";
+    {
+        cap.read(frame);
+        if(frame.empty())
+        {
+            cerr << "ERROR! Blank frame grabbed" << endl;
+            break;
         }
-		std::cout << std::endl;
+
+        // Mats to hold grayscale and edge detection results
+        cv::Mat gray;
+        cv::Mat edges;
+
+        // pass frame to detector here
+        std::vector<AR::Tag> tags =
+        AR::findTags(frame, gray, edges, thresh_val, thresh2_val, blur_val);
+
+        // show locations of tags in window
+        std::cout << "Found " << tags.size() << " tags" << std::endl;
+
+        // draw lines between tag points
+        for(AR::Tag tag : tags)
+        {
+            std::vector<AR::Corner> corners = tag.getCorners();
+            corners.push_back(corners[0]);
+            for(int i = 0; i < corners.size() - 1; i++)
+            {
+                cv::line(frame, corners[i].point, corners[i + 1].point, cv::Scalar(0, 0, 255), 3);
+            }
+            std::cout << tag.getCenter() << ", ";
+        }
+        std::cout << std::endl;
 
         cv::imshow(ORIG_WINDOW_NAME, frame);
-		if(EXTRA_WINDOWS)
+        if(EXTRA_WINDOWS)
         {
             cv::imshow("Gray", gray);
             cv::imshow("Edges", edges);
         }
 
         if(cv::waitKey(5) == 'q')
-			break;
-	}
-	return 0;
+            break;
+    }
+    return 0;
 }
