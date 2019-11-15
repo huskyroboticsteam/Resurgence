8303c22cd6b21996e704bd082c9786503e7323d7
Evan Straw <evan.straw99@gmail.com>
2019-11-15 14:56:12 -0800

    Create first working version of AR Tag detection (#12)
    * Add OpenCV back to CMakeLists
    
    OpenCV is needed for the AR detection code, and will likely be needed
    for other parts of the project as well, so I've added it back to the
    CMake build. OpenCV isn't too hard to build on GNU/Linux systems, and
    I think there's a pre-compiled version you can install on Windows
    systems.
    
    * Add temporary fix to get project to compile
    
    * Add ar subdirectory for AR tag detection code
    
    I added ar as a subdirectory because I need a separate binary for
    testing AR detection to be compiled. This binary doesn't affect the
    rest of the code, all of its code will be contained in
    src/ar/ARTester.cpp, and I'll make sure it compiles before pushing
    anything.
    
    I also feel like having the AR stuff in a subdirectory kind of helps
    to separate it from the rest of the project.
    
    * Add somewhat working version of the tester/detector
    
    * Add subdirectories
    
    * Fix ObstacleSim so that the project compiles
    
    * Add AR tag detection headers
    
    * Add findAngle function
    
    * Add intrinsic camera parameters
    
    * Create slightly working tester program
    
    * Add field for pitch
    
    * add getter method in tag header file
    
    * Add Tag constructor
    
    * added getCorners and calcOrientation
    
    * fix blank
    
    * Fix bugs with and finish calcOrientation
    
    * getPitch Yaw and Roll
    
    * Fix issue with solvePnP
    
    * Fix issue with point ordering
    
    * add code to draw lines along tag
    
    * add code to draw lines along tag
    
    * Improve detection process
    
    Improved detection by only returning outside contours, preventing
    detection of smaller shapes inside the AR tag.
    
    * add getCenter and supporting methods
    
    * Fix bugs with Tag
    
    * Create somewhat working version of detection.
    
    Still needs a lot of improvements, but this commit contains a working
    version of the detection and orientation calculation process.
    
    Areas that now need fine-tuning: filtering out what are and are not
    tags in the detection process. Probably will try to read the tag data
    and if it cannot be done, shape is probably not a tag
    
    * Add documentation.
    
    * Fix some issues with tabs and spaces
    
    * Add detailed installation instructions to README
    
    * Fix most issues mentioned in pull request review
    
    Only issue that is not fixed so far is the error code issue, as I'm
    not sure yet how to implement that. Will add a fix for that later.
    
    * Remove the use of exceptions as per pull request review
    
    * Fix more style-related issues mentioned in code review

---
+++ src/ar/ARTester.cpp
@@ -0,0 +1,93 @@
+#include "Detector.h"
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <iostream>
+
+const std::string ORIG_WINDOW_NAME = "Image (Original)";
+const std::string THRESH_TRACKBAR_NAME = "Threshold";
+const std::string THRESH2_TRACKBAR_NAME = "Threshold 2";
+const std::string BLUR_TRACKBAR_NAME = "Blur";
+
+constexpr bool EXTRA_WINDOWS = true;
+
+constexpr int CAMERA_ID = 0;
+
+int main()
+{
+	cv::Mat frame;
+	cv::VideoCapture cap;
+
+	int api_id = cv::CAP_ANY;
+
+	int thresh_val = 128;
+	int thresh2_val = 128;
+	int blur_val = 2;
+
+	std::cout << "Opening camera..." << std::endl;
+
+	cap.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
+	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+
+	cap.open(CAMERA_ID + api_id);
+	if (!cap.isOpened())
+	{
+		std::cerr << "ERROR! Unable to open camera" << std::endl;
+		return 1;
+	}
+
+	std::cout << "Opening image window, press Q to quit" << std::endl;
+
+	cv::namedWindow(ORIG_WINDOW_NAME);
+	cv::createTrackbar(THRESH_TRACKBAR_NAME, ORIG_WINDOW_NAME, &thresh_val, 256);
+	cv::createTrackbar(THRESH2_TRACKBAR_NAME, ORIG_WINDOW_NAME, &thresh2_val, 256);
+	cv::createTrackbar(BLUR_TRACKBAR_NAME, ORIG_WINDOW_NAME, &blur_val, 10);
+
+	while (true)
+	{
+		cap.read(frame);
+		if (frame.empty())
+		{
+			std::cerr << "ERROR! Blank frame grabbed" << std::endl;
+			break;
+		}
+
+		// Mats to hold grayscale and edge detection results
+		cv::Mat gray;
+		cv::Mat edges;
+
+		// pass frame to detector here
+		std::vector<AR::Tag> tags =
+		    AR::findTags(frame, gray, edges, thresh_val, thresh2_val, blur_val);
+
+		// show locations of tags in window
+		std::cout << "Found " << tags.size() << " tags" << std::endl;
+
+		// draw lines between tag points
+		for (AR::Tag tag : tags)
+		{
+			std::vector<AR::Corner> corners = tag.getCorners();
+			corners.push_back(corners[0]);
+			for (int i = 0; i < corners.size() - 1; i++)
+			{
+				cv::line(frame, corners[i].point, corners[i + 1].point, cv::Scalar(0, 0, 255),
+				         3);
+			}
+			std::cout << tag.getCenter() << ", ";
+		}
+		std::cout << std::endl;
+
+		cv::imshow(ORIG_WINDOW_NAME, frame);
+		if (EXTRA_WINDOWS)
+		{
+			cv::imshow("Gray", gray);
+			cv::imshow("Edges", edges);
+		}
+
+		if (cv::waitKey(5) == 'q')
+			break;
+	}
+	return 0;
+}
