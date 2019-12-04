fa00bb230014afb1342adb1344713bf21853bbe5
assafvayner <assafvayner@gmail.com>
2019-12-03 19:28:12 -0800

    one change

--- src/Pathfinding/testMain.cpp
+++ src/Pathfinding/testMain.cpp
@@ -12,26 +12,11 @@ int main()
     {
         float x,y;
         envmap.getRobotPosition(x,y);
-        x += j;
-        y += j;
 
         m.points.push_back(Vec2{x,y});
     }
     MapObstacle& n = m;
     envmap.newObstacleUID(m);
-
-
-    // MapObstacle proto_obst;
-    // proto_obst.position.x = 1.0f;
-    // proto_obst.position.y = - 1.0f;
-    // size_t obst_uid_1 = envmap.newObstacleUID(proto_obst);
-
-    // proto_obst.position.x = - 1.5f;
-    // proto_obst.position.y = - 6.0f;
-    // size_t obst_uid_2 = envmap.newObstacleUID(proto_obst);
-
-    // std::shared_ptr<MapObstacle> obst_1_ptr = map.getObstacle(obst_uid_1);
-    // std::shared_ptr<MapObstacle> obst_2_ptr = map.getObstacle(obst_uid_2);
 
     // create obstacleMap using envmap
     ObstacleMap map = ObstacleMap(envmap);
