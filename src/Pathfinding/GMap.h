fe74133562e6cb0f7079816a7944daf86882c2ce
assafvayner <assafvayner@gmail.com>
2020-01-08 19:21:53 -0800

    changes to GMap

--- src/Pathfinding/GMap.h
+++ src/Pathfinding/GMap.h
@@ -1,20 +1,24 @@
 #pragma once
 #include <vector>
+//include "ekfslam.h"
 
 class GMap
 {
     //GMap();
+    // add fields for robot location + get/set
 private:
-    std::vector<Point> globalObstacleList;
-    reduceObstacles();
+    std::vector<Point&> globalObstacleList;
 
 public:
-    addSLAMData();//ask slam for data type to be input
-    getObstaclesWithinSquare(int squareRad, int x, int y);
+    int addPoint(Point& point);//ask slam for data type to be input
+    Point& getPointByID(int id);
+    std::vector<Point&> getObstaclesWithinSquare(int squareRad, int x, int y);
+    void getRobotPosition(int& x, int& y)
 }
 
 struct Point
 {
     int x;
     int y;
+    int id;
 }
