86911f7a02c498e412ba2c12ba75f8b56eac2b29
Alex <alexcalbert@yahoo.com>
2020-01-11 14:51:33 -0800

    Created Pather2.h and updated Pather2.cpp to interface better

--- src/Pathfinding/Pather2.cpp
+++ src/Pathfinding/Pather2.cpp
@@ -1,15 +1,15 @@
-#include "Map.h"
-#include "Pather.h"
+#include "ObstacleMap.h"
+#include "Pather2.h"
 #include <vector>
 #include <queue>
 #include <iostream>
 
 //matrix size
-#define ROW 5
-#define COL 5
+//#define ROW 5
+//#define COL 5
 
-void print_point(std::queue<Pather::point> q)
-{
+
+void print_point(std::queue<Pather2::point> q) {
   while (!q.empty())
   {
     std::cout << "(" << q.front().x << ",";
@@ -19,12 +19,10 @@ void print_point(std::queue<Pather::point> q)
   std::cout << std::endl;
 }
 
-void print_queue(std::queue<Pather::queueNode> q)
-{
+void print_queue(std::queue<Pather2::queueNode> q) {
   while (!q.empty())
   {
-    std::cout << "Hello";
-    std::queue<Pather::point> temp = q.front().path;
+    std::queue<Pather2::point> temp = q.front().path;
     print_point(temp);
     std::cout << std::endl;
     q.pop();
@@ -40,48 +38,52 @@ bool isValid(int x, int y) {
 int rowNum[] = {-1,0,0,1};
 int rowCol[] = {0,-1,1,0};
 
-Pather::point BFS(int mat[ROW][COL], Pather::point src, Pather::point dest){
+Pather2::point getPath(bool[][] map, Pather2::point dest){
+
+    Pather2::point src = {10,10};
 
     //check if src and dest are represented with 1 not 0
-    if (!mat[src.x][src.y] || !mat[dest.x][dest.y]){
-        struct Pather::point error = {-1, -1};
+    if (map[src.x][src.y] || map[dest.x][dest.y]){
+        struct Pather2::point error = {-1, -1};
+        //todo: change destination if destination is blocked
         return error;
     }
 
     //create 2D boolean array and use memset to set all values as false
-    bool visited[ROW][COL];
-    memset(visited, false, sizeof visited); 
+    //bool visited[21][21];
+    //memset(visited, false, sizeof visited); 
 
-    for (int i = 0; i < ROW; i++) {
-        for (int j = 0; j < COL; j++) {
-            if (mat[i][j] == 0) {
-                visited[i][j] = true;
-            }
-        }
-    } 
+    // for (int i = 0; i < ROW; i++) {
+    //     for (int j = 0; j < COL; j++) {
+    //         if (map[i][j]) {
+    //             visited[i][j] = true;
+    //         }
+    //     }
+    // } 
 
     //set source as visited, create node with dist 0 from src and push to queue of nodes to be checked
-    visited[src.x][src.y] = true;
-    std::queue<Pather::queueNode> q;
-    std::queue<Pather::point> newQueue;
-    Pather::queueNode s = {src, 0, newQueue};
+    //visited[src.x][src.y] = true;
+    map[src.x][src.y] = true;
+    std::queue<Pather2::queueNode> q;
+    std::queue<Pather2::point> newQueue;
+    Pather2::queueNode s = {src, 0, newQueue};
     q.push(s);
 
     
 
     while (!q.empty()){
-        print_queue(q);
         //get front node in nodes to be checked and make it current
-        Pather::queueNode curr = q.front();
-        Pather::point pt = curr.pt;
+        Pather2::queueNode curr = q.front();
+        Pather2::point pt = curr.pt;
 
         //check if current node is destination node
         if ((pt.x == dest.x) && (pt.y == dest.y)){
             //return first node after source in path to destination
-            std::queue<Pather::point> temp = curr.path;
+            std::queue<Pather2::point> temp = curr.path;
             temp.pop();
-            std::cout << "Hi" << std::endl;
-            return temp.front();
+            std::cout << "Final path: ";
+            print_queue(q);
+            return temp.front(); 
         }
 
         //pop current node off nodes to be checked
@@ -94,12 +96,12 @@ Pather::point BFS(int mat[ROW][COL], Pather::point src, Pather::point dest){
             
 
             //check if adj node is not out of bounds and is not an obstacle
-            if (isValid(row, col) && !visited[row][col]){
+            if (isValid(row, col) && !map[row][col]){
                 //set adj node as visited and push it to nodes to be checked
-                struct Pather::point test = {row, col};
+                struct Pather2::point test = {row, col};
                 curr.path.push(test);
-                Pather::queueNode adjNode = {{row, col}, curr.dist + 1, curr.path};
-                visited[adjNode.pt.x][adjNode.pt.y] = true;
+                Pather2::queueNode adjNode = {{row, col}, curr.dist + 1, curr.path};
+                map[adjNode.pt.x][adjNode.pt.y] = true;
                 q.push(adjNode);
             }
         }
@@ -108,17 +110,22 @@ Pather::point BFS(int mat[ROW][COL], Pather::point src, Pather::point dest){
     return src;
 }
 
-int main () {
+Pather2::Pather2(ObstacleMap obstacle_map)
+{
+    map = obstacle_map;   
+}
+
+int main() {
     int map[ROW][COL] = {{0, 1, 0, 1, 0},
-               {0, 0, 0, 0, 1},
-               {0, 1, 1, 0, 0},
-               {0, 1, 1, 0, 1},
-               {1, 1, 1, 1, 1}};
-    struct Pather::point src = {2, 2};
-    struct Pather::point destination = {4, 4};
-    struct Pather::point a = BFS(map, src, destination);
-    std::cout << a.x;
-    std::cout << a.y;
+                         {0, 0, 0, 0, 1},
+                         {0, 1, 1, 1, 1},
+                         {0, 1, 1, 1, 1},
+                         {1, 1, 1, 1, 1}};
+    struct Pather2::point src = {2, 2};
+    struct Pather2::point destination = {4, 4};
+    struct Pather2::point nextPoint = getPath(map, src, destination);
+    std::cout << "First point in path: ";
+    std::cout << "(" << nextPoint.x << "," << nextPoint.y << ")";
     std::cout << "" << std::endl;
     return 0;
 }
---
+++ src/Pathfinding/Pather2.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Geometry.h"
+#include <vector>
+#include <queue>
+#include <string>
+#include "ObstacleMap.h"
+class Pather2 {
+private:
+    bool[ObstacleMap.size][ObstacleMap.size] map;
+
+    point getPath(int mat[][], point dest);
+
+public:
+    struct point {
+        int x;
+        int y;
+    };
+            
+    struct queueNode {
+    //for use in BFS search
+        point pt;
+        int dist;
+        std::queue<point> path;
+    };
+};
