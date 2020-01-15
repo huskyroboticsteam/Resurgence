30e07af1246c13b3b55c042a6fc2b939890468b2
assafvayner <assafvayner@gmail.com>
2020-01-14 19:54:49 -0800

    modified comments and removed constructor

--- src/Pathfinding/ObstacleMap.h
+++ src/Pathfinding/ObstacleMap.h
@@ -2,25 +2,22 @@
 #include <vector>
 #include <math.h>
 #include <iostream>
+
 // todo: include Point struct located in slam files
 
-// use asserts
-
-//make a graph object for pathing
-//use 2d array not vectors
-
-//make reference or pointer to global SLAM map obj
-// recieve list of obstacles within specified range
+// recieve list of points
 // plot obstacles onto 2d grid
 // pass grid onto pathing
 // recreate grid as orientation/location changes
 
-//increase bounding box 
+//uses default constructor with no params
+
 class ObstacleMap{
-    constexpr static int radius = 10.0;
-    constexpr static int step_size = 1.0;
+    constexpr static int radius = 10;
+    constexpr static int step_size = 1;
     constexpr static int size = 21;//(int)(2 * radius + 1);
     bool obstacle_map[size][size];
+
 
 private:
     void resetObstacleMap();
@@ -29,7 +26,5 @@ private:
 
 public:
     void update(std::vector<Point&> obstacles);
-    ObstacleMap(std::vector<Point&> obstacles);
     void print();
-    bool[][]& getMap();
 };
