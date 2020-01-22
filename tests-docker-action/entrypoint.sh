454901762a611e6548f04707c4404a728bed6072
andrewbriand <atb8888@comcast.net>
2020-01-21 18:38:47 -0800

    Change cmake command

--- tests-docker-action/entrypoint.sh
+++ tests-docker-action/entrypoint.sh
@@ -5,6 +5,8 @@ cmake -B./build -H. -DBUILD_TESTING=OFF
 cmake --build ./build/ --target install
 cd ..
 mkdir ./build
-cmake -B./build ./src
+cd build
+cmake ../src
+cd ..
 cmake --build ./build
 ./build/tests
