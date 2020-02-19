9a62b2bd3500283ef018f83a246e48c22df13c99
kinjal haldar <kinjal.haldar@gmail.com>
2020-02-18 19:56:25 -0800

    fixed std::chrono thing by using the full namespace istead of auto

--- src/lidar/URGLidar.h
+++ src/lidar/URGLidar.h
@@ -14,21 +14,22 @@ typedef struct Polar2D
     double r, theta;
     int asCartesianX()
     {
-        (long)(r*cos(theta));
+        (long)(r * cos(theta));
     }
     int asCartesianY()
     {
-        (long)(r*sin(theta));
+        (long)(r * sin(theta));
     }
 } Polar2D;
 
-class URGLidar {
+class URGLidar
+{
 private:
     urg_t urg;
     int error = 0;
     const char connect_device[13] = "/dev/ttyACM0";
     const long connect_baudrate = 115200;
-    std::vector<std::shared_ptr<Polar2D>> lastFrame; //The last completed frame.
+    std::vector<std::shared_ptr<Polar2D>> lastFrame;                  //The last completed frame.
     std::chrono::time_point<std::chrono::steady_clock> lastFrameTime; //The system time at which the last frame was completed.
 
 public:
@@ -51,7 +52,7 @@ public:
      * last queried for a new frame, or the time this URGLidar was
      * created if no frame have been created.
      */
-    auto getLastFrameTime();
+    std::chrono::time_point<std::chrono::steady_clock> getLastFrameTime();
     /**
      * Builds the next frame and updates the last frame and time variables
      * @returns True if this operation was successful.
