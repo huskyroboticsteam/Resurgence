673a7ba5d018c16596692068ba8dbe96b7849270
andrewbriand <atb8888@comcast.net>
2020-02-18 18:58:31 -0800

    Try to add urg library

--- tests-docker-action/entrypoint.sh
+++ tests-docker-action/entrypoint.sh
@@ -1,4 +1,9 @@
 echo "test"
+sudo apt-get install -y mercurial
+hg clone http://hg.code.sf.net/p/urgnetwork/urg_library 
+cd urg_library
+make
+make install
 git clone https://github.com/catchorg/Catch2.git
 cd ./Catch2
 cmake -B./build -H. -DBUILD_TESTING=OFF
