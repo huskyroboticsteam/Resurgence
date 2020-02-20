8f5266a5c61528b119108495d7e7553620ce951f
Nathan Hatch <nathanhatch297@gmail.com>
2020-02-19 19:32:12 -0800

    Implement sending mode change CAN packets

--- src/Networking/ParseBaseStation.cpp
+++ src/Networking/ParseBaseStation.cpp
@@ -2,6 +2,14 @@
 #include "Network.h"
 #include "../Globals.h"
 #include "json.hpp"
+#include "CANUtils.h"
+#include "ParseCAN.h"
+
+extern "C"
+{
+    #include "../HindsightCAN/CANMotorUnit.h"
+}
+
 #include <iostream>
 
 using nlohmann::json;
@@ -9,7 +17,17 @@ using nlohmann::json;
 json motor_status = {};
 
 void ParseMotorPacket(json &message);
-void SendCANPacketToMotor(json &message, std::string &key);
+bool SendCANPacketToMotor(json &message, std::string &key, int motor_serial);
+
+int getIndex(const std::vector<std::string> &arr, std::string &value)
+{
+  for (int i = 0; i < arr.size(); i++) {
+    if (arr[i] == value) {
+      return i;
+    }
+  }
+  return -1;
+}
 
 void ParseBaseStationPacket(char* buffer)
 {
@@ -27,6 +45,12 @@ void ParseBaseStationPacket(char* buffer)
 void ParseMotorPacket(json &message)
 {
   std::string motor = message["motor"];
+  int motor_serial = getIndex(motor_group, motor);
+  if (motor_serial < 0)
+  {
+    std::cout << "Unrecognized motor " << motor << std::endl;
+    return;
+  }
   std::cout << "Parsing motor packet for motor " << motor << std::endl;
   motor_status[motor]["motor"] = motor;
   std::cout << "Original status: " << motor_status[motor] << std::endl;
@@ -35,27 +59,39 @@ void ParseMotorPacket(json &message)
 
   for (std::string key : possible_keys) {
     if (message[key] != nullptr && message[key] != motor_status[motor][key]) {
-      // TODO input validation: e.g. if this is a PID target, make sure message[key] is
-      // an integer. Or maybe this could be handled in a subroutine
-      motor_status[motor][key] = message[key];
       std::cout << "Updating " << key << std::endl;
-      SendCANPacketToMotor(message, key);
+      if (SendCANPacketToMotor(message, key, motor_serial)) {
+        motor_status[motor][key] = message[key];
+      } else {
+        std::cout << "Failed to send CAN packet.\n";
+      }
     }
   }
 
   std::cout << "Final status: " << motor_status[motor] << std::endl << std::endl;
 }
 
-void SendCANPacketToMotor(json &message, std::string &key) {
+bool SendCANPacketToMotor(json &message, std::string &key, int motor_serial) {
   if (key == "mode") {
     std::string mode = message[key];
+    uint16_t CAN_ID = ConstructCANID(PACKET_PRIORITY_NORMAL,
+                                     DEVICE_GROUP_MOTOR_CONTROL,
+                                     motor_serial);
+    uint8_t length = 0x02;
+    uint8_t data[length];
+    WritePacketIDOnly(data, ID_MOTOR_UNIT_MODE_SEL);
+
     if (mode == "PWM") {
-      // TODO: Send CAN packet changing the mode to PWM
-      // See TestPackets.cpp and CANUtils.cpp and FakeCANBoard.cpp for examples
+      data[1] = MOTOR_UNIT_MODE_PWM;
     } else if (mode == "PID") {
-      // TODO: Send CAN packet changing the mode to PID
+      data[1] = MOTOR_UNIT_MODE_PID;
     } else {
       std::cout << "Error: unrecognized mode " << mode << std::endl;
+      return false;
     }
+    CANPacket p = ConstructCANPacket(CAN_ID, length, data);
+    SendCANPacket(p);
   }
+
+  return true;
 }
