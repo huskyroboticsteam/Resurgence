f310adc29e0708fd8e7eb28ada7fad074d7d033b
Stephanie Osorio <steph.osoriot@gmail.com>
2020-02-26 19:50:48 -0800

    Simulator drawMap rectangle drawing

--- src/Pathfinding/Simulator.cpp
+++ src/Pathfinding/Simulator.cpp
@@ -1,17 +1,29 @@
-#include Simulator.h
+#include "Simulator.h"
 
 int main(void ){
     
 };
 
-void drawMap(bool obstacle_map[][]){
+void Simulator::drawMap(bool obstacle_map[][21]){
+    
+    for(int i; i < size; i++){
+        for(int j; j < size; j++){
+            if(obstacle_map[i][j]){
+                  rectangle(img,
+                    Point( w/21*i,w/21*j  ),
+                    Point( w/21*(i+1), w/21*(j+1)),
+                    Scalar( 255, 255, 255 ),
+                    FILLED,
+                    LINE_8 );
+            }
+        }
+    }
+};
+
+void Simulator::drawPath(){
 
 };
 
-void drawPath(){
-
-};
-
-void interpretCoordinates(){
+void Simulator::interpretCoordinates(){
 
 };
