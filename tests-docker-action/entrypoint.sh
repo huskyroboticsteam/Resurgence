962eeef4f4ab1baa1102889043c7415b457d7fc7
Nathan Hatch <nathanhatch297@gmail.com>
2020-06-20 16:26:16 -0700

    And again

--- tests-docker-action/entrypoint.sh
+++ tests-docker-action/entrypoint.sh
@@ -3,9 +3,10 @@ cd urg_library-1.2.5
 make
 make install
 cd ..
-apt-get install libsfml-dev
+apt-get install -y libsfml-dev
 # This next line is apparently where Eigen is installed
 export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:`pwd`src/mapping/EKFSlam
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/include/eigen3
 git submodule update --init --recursive
 git clone https://github.com/catchorg/Catch2.git
 cd ./Catch2
