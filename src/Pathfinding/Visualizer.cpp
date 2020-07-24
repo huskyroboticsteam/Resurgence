829ee0520290d33b6f424b51ed973bf237291960
Nathan Hatch <nathanhatch297@gmail.com>
2020-07-23 17:47:34 -0700

    Update to new API

--- src/Pathfinding/Visualizer.cpp
+++ src/Pathfinding/Visualizer.cpp
@@ -1,5 +1,6 @@
 #include "Visualizer.h"
 #include "ObstacleMap.h"
+#include <iostream>
 
 using namespace cv;
 
@@ -9,7 +10,7 @@ void runTest(std::string description, ObstacleMap testMap, PointXY dest) {
     std::cout << "Text version of map:\n";
     testMap.print();
     sim.drawMap(testMap.obstacle_map);
-    std::queue<PointXY> path = pather.BFS(testMap.obstacle_map, dest);
+    std::queue<PointXY> path = pather.BFS(dest);
     sim.drawPath(path);
     sim.drawRobot();
     sim.drawDestination();
@@ -23,7 +24,7 @@ int main(void) {
     // Test1:
     ObstacleMap testMap1;
     std::vector<PointXY> points1 = {}; // supposed to be PointXY
-    testMap1.update(points1);
+    testMap1.update(points1, 0, 0);
     runTest("empty map", testMap1, PointXY{10.0f, 10.0f});
 
     // Test2:
@@ -40,7 +41,7 @@ int main(void) {
         PointXY{9.0f, 9.0f},
         PointXY{10.0f, 10.0f}
     };
-    testMap2.update(points2);
+    testMap2.update(points2, 0, 0);
     runTest("diagonal obstacles", testMap2, PointXY{10.0f, 10.0f});
 
     // Test3:
@@ -53,7 +54,7 @@ int main(void) {
         PointXY{5.0f, -5.0f},
         PointXY{1.0f, 1.0f}
     };
-    testMap3.update(points3);
+    testMap3.update(points3, 0, 0);
     runTest("irregular obstacles", testMap3, PointXY{10.0f, 10.0f});
 
     // Test4:
@@ -70,7 +71,7 @@ int main(void) {
         PointXY{9.0f, 9.0f},
         PointXY{10.0f, 10.0f}
     };
-    testMap4.update(points4);
+    testMap4.update(points4, 0, 0);
     runTest("different goal", testMap4, PointXY{10.0f, 7.0f});
 
     // Test5:
@@ -87,7 +88,7 @@ int main(void) {
         PointXY{9.0f, 9.0f},
         PointXY{10.0f, 10.0f}
     };
-    testMap5.update(points5);
+    testMap5.update(points5, 0, 0);
     runTest("another different goal", testMap5, PointXY{7.0f, 10.0f});
 
     // Test6:
@@ -104,7 +105,7 @@ int main(void) {
         PointXY{6.0f, 4.0f},
         PointXY{1.0f, 3.0f}
     };
-    testMap6.update(points6);
+    testMap6.update(points6, 0, 0);
     runTest("more irregular obstacles", testMap6, PointXY{5.0f, 5.0f});
 }
 
