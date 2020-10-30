aa8e6691a65097c6fa7fc6cd5d291d027eba5d6f
Abhay D <a.deshpande012@gmail.com>
2020-10-30 15:39:58 -0700

    Added more comments to code

--- src/filters/PoseEstimator.cpp
+++ src/filters/PoseEstimator.cpp
@@ -3,8 +3,6 @@
 #include <Eigen/LU>
 #include <unsupported/Eigen/MatrixFunctions>
 
-#include "../simulator/utils.h"
-
 typedef Eigen::Matrix3d matrix;
 typedef Eigen::Vector3d vector;
 
@@ -17,32 +15,31 @@ matrix createCovarianceMatrix(const vector &stdDevs)
 
 void discreteToContinuous(matrix &A, matrix &B, double dt)
 {
+	// Reference paper: https://doi.org/10.1016/0307-904X(80)90177-8
 	matrix G = A;
 	matrix H = B;
 	A = G.log() / dt;
 	B = A * (G - Eigen::Matrix3d::Identity()).inverse() * H;
 }
 
-// contQ is covariance matrix
+// contA is continuous system matrix, contQ is continuous process covariance matrix
 matrix discretizeQ(const matrix &contA, const matrix &contQ, double dt)
 {
+	// implements zero order hold discretization of the system and state covariance matrices
+	// reference: https://en.wikipedia.org/wiki/Discretization#Discretization_of_process_noise
 	Eigen::Matrix<double, 6, 6> M;
 	M.block<3, 3>(0, 0) = -contA;
 	M.block<3, 3>(0, 3) = contQ;
 	M.block<3, 3>(3, 0).setZero();
 	M.block<3, 3>(3, 3) = contA.transpose();
 
-	Eigen::Matrix<double, 6, 6> phi = (M * dt).exp();
+	Eigen::Matrix<double, 6, 6> G = (M * dt).exp();
 
-	// Phi12 = phi[0:States,        States:2*States]
-	// Phi22 = phi[States:2*States, States:2*States]
-	Eigen::Matrix3d phi12 = phi.block(0, 3, 3, 3);
-	Eigen::Matrix<double, 3, 3> phi22 = phi.block(3, 3, 3, 3);
+	matrix AinvQ = G.block(0, 3, 3, 3); // this is discA^-1 * Q
+	matrix discA = G.block(3, 3, 3, 3).transpose();
 
-	matrix discA = phi22.transpose();
-
-	matrix q = discA * phi12;
-	q = (q + q.transpose()) / 2.0;
+	matrix q = discA * AinvQ;	   // A * A^-1 * Q = Q
+	q = (q + q.transpose()) / 2.0; // make Q symmetric again if it became asymmetric
 
 	return q;
 }
@@ -53,11 +50,12 @@ matrix discretizeR(const matrix &contR, double dt)
 	return contR / dt;
 }
 
-// Solves Discrete-time Algebraic Riccati Equation
-// reference:
-// https://scicomp.stackexchange.com/questions/30757/discrete-time-algebraic-riccati-equation-dare-solver-in-c
+// Solves Discrete-time Algebraic Riccati Equation to calculate asymptotic error covariance
+// matrix This can be used to calculate the optimal Kalman gain matrix
 matrix DARE(const matrix &A0, const matrix &B, const matrix &Q, const matrix &R)
 {
+	// reference:
+	// https://scicomp.stackexchange.com/questions/30757/discrete-time-algebraic-riccati-equation-dare-solver-in-c
 	matrix A = A0;
 	matrix G = B * R.inverse() * B.transpose();
 	matrix H = Q;
@@ -95,12 +93,14 @@ PoseEstimator::PoseEstimator(const Eigen::Vector3d &stateStdDevs,
 	stateCovariance = discretizeQ(contA, stateCovarianceCont, dt);
 	measurementCovariance = discretizeR(measurementCovarianceCont, dt);
 
-	// solve DARE for state error covariance matrix
-	matrix P = DARE(systemMat.transpose(), outputMat.transpose(), stateCovariance, measurementCovariance);
+	// solve DARE for asymptotic state error covariance matrix
+	matrix P = DARE(systemMat.transpose(), outputMat.transpose(), stateCovariance,
+					measurementCovariance);
 
 	matrix S = outputMat * P * outputMat.transpose() + measurementCovariance;
 	// This is the Kalman gain matrix, used to weight the GPS data against the model data
-	gainMatrix = S.transpose().colPivHouseholderQr().solve((outputMat * P.transpose()).transpose());
+	gainMatrix =
+		S.transpose().colPivHouseholderQr().solve((outputMat * P.transpose()).transpose());
 }
 
 vector getPoseDiff(const vector &pose, double dt, double thetaVel, double xVel)
@@ -116,10 +116,12 @@ void PoseEstimator::predict(double thetaVel, double xVel)
 {
 	vector u = getPoseDiff(xHat, dt, thetaVel, xVel);
 	xHat = systemMat * xHat + inputMat * u;
-	estimateCovariance = systemMat * estimateCovariance * systemMat.transpose() + stateCovariance;
+	estimateCovariance =
+		systemMat * estimateCovariance * systemMat.transpose() + stateCovariance;
 }
 
-void PoseEstimator::correct(const transform_t &measurement) {
+void PoseEstimator::correct(const transform_t &measurement)
+{
 	pose_t pose = toPose(measurement, xHat[2]);
 	xHat = xHat + gainMatrix * (pose - outputMat * xHat);
 	estimateCovariance = (matrix::Identity() - gainMatrix * outputMat) * estimateCovariance;
