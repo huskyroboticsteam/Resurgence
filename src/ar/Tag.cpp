5a50cf825a94cd5f75eb4e06fc41faac4a86f8ac
Evan Straw <evan.straw99@gmail.com>
2020-11-18 17:43:16 -0800

    Remove unnecessary methods from Tag

--- src/ar/Tag.cpp
+++ src/ar/Tag.cpp
@@ -11,113 +11,27 @@
 namespace AR
 {
 
-Tag::Tag(cv::Point top_left, cv::Point top_right, cv::Point bottom_right,
-		 cv::Point bottom_left, CameraParams params, TagID tag_id)
-	: params(params)
-{
-	// validate points
-	// TODO determine if checkcorners is actually necessary
-	//	checkCorners(top_left, top_right, bottom_right, bottom_left);
-
-	// fill vector with points
-	corners.push_back(top_left);
-	corners.push_back(top_right);
-	corners.push_back(bottom_right);
-	corners.push_back(bottom_left);
-
-	id = tag_id;
-
-	calcOrientation();
-}
-
-cv::Point getTriCenter(cv::Point pt1, cv::Point pt2, cv::Point pt3)
-{
-	double centroidX = (pt1.x + pt2.x + pt3.x) / 3;
-	double centroidY = (pt1.y + pt2.y + pt3.y) / 3;
-	return cv::Point2d(centroidX, centroidY);
-}
-
-cv::Point getQuadCenter(cv::Point pt1, cv::Point pt2, cv::Point pt3, cv::Point pt4)
-{
-	double centroidX = (pt1.x + pt2.x + pt3.x + pt4.x) / 4;
-	double centroidY = (pt1.y + pt2.y + pt3.y + pt4.y) / 4;
-	return cv::Point2d(centroidX, centroidY);
-}
-
-cv::Point Tag::getCenter() const
-{
-	std::vector<cv::Point> tri_centers;
-	for (size_t i = 0; i < 4; i++)
-	{
-		size_t next = (i == 3 ? 0 : i + 1);
-		size_t prev = (i == 0 ? 3 : i - 1);
-		tri_centers.push_back(getTriCenter(corners[prev], corners[i], corners[next]));
-	}
-	return getQuadCenter(tri_centers[0], tri_centers[1], tri_centers[2], tri_centers[3]);
-}
-
-std::vector<cv::Point> Tag::getCorners() const
-{
-	return corners;
-}
-
-void Tag::calcOrientation()
-{
-	// vectors to hold image points (detected corners in image) and object points ("ideal"
-	// corners of the tag in the world coordinate system)
-	std::vector<cv::Point2f> image_points;
-	std::vector<cv::Point3f> object_points;
-
-	// add detected corners to image points vector
-	for (int i = 0; i < corners.size(); i++)
-	{
-		image_points.push_back(corners[i]);
-	}
-
-	// create ideal object points
-	double square_len = 0.2;
-	// top left (-w/2, w/2)
-	object_points.push_back(cv::Point3f(-(square_len / 2), (square_len / 2), 0));
-	// top right (w/2, w/2)
-	object_points.push_back(cv::Point3f((square_len / 2), (square_len / 2), 0));
-	// bottom right (w/2, -w/2)
-	object_points.push_back(cv::Point3f((square_len / 2), -(square_len / 2), 0));
-	// bottom left (-w/2, -w/2)
-	object_points.push_back(cv::Point3f(-(square_len / 2), -(square_len / 2), 0));
-
-	// std::cout << "object points: " << object_points << std::endl;
-
-	// Mat objects to hold returned rotation and translation vectors
-	cv::Mat _rvec;
-	cv::Mat _tvec;
-
-	// estimate pose
-	cv::solvePnP(object_points, image_points, params.getCameraParams(), params.getDistCoeff(),
-				 _rvec, _tvec, false, cv::SOLVEPNP_IPPE_SQUARE);
-
-	// store rotation and translation vectors in this tag instance
-	rvec = _rvec;
-	tvec = _tvec;
-}
-
-TagID Tag::getID() const
-{
-	return this->id;
-}
+Tag::Tag(Marker marker, cv::Vec3d rvec, cv::Vec3d tvec)
+	: marker_(marker), rvec_(rvec), tvec_(tvec){}
 
 cv::Vec3d Tag::getRVec() const
 {
-	return rvec;
+	return rvec_;
 }
 
 cv::Vec3d Tag::getTVec() const
 {
-	return tvec;
+	return tvec_;
 }
 
 cv::Vec3d Tag::getCoordinates() const
 {
-	return tvec;
+	return tvec_;
+}
+
+Marker Tag::getMarker() const
+{
+	return marker_;
 }
 
 } // namespace AR
--- src/ar/Tag.h
+++ src/ar/Tag.h
@@ -5,42 +5,10 @@
 #include <opencv2/core.hpp>
 
 #include "CameraParams.h"
+#include "MarkerSet.h"
 
 namespace AR
 {
-
-/**
-   @brief Enum containing values for the corner indices.
-
-   This is mostly used internally but may be used as a reference to see what the correct
-   corner ordering is. The order currently goes clockwise starting with top left.
- */
-enum CornerIndex
-{
-	TOP_LEFT = 0,
-	TOP_RIGHT = 1,
-	BOTTOM_RIGHT = 2,
-	BOTTOM_LEFT = 3
-};
-
-/**
-   Tag ID enum. Each member's value is the numerical ID of the tag it stands for.
- */
-enum TagID
-{
-	ID_UNKNOWN = -1,
-	ID_LEG_1 = 0,
-	ID_LEG_2 = 10,
-	ID_LEG_3 = 11,
-	ID_LEG_4L = 1,
-	ID_LEG_4R = 2,
-	ID_LEG_5L = 8,
-	ID_LEG_5R = 9,
-	ID_LEG_6L = 3,
-	ID_LEG_6R = 12,
-	ID_LEG_7L = 6,
-	ID_LEG_7R = 7
-};
 
 /**
    @brief Class representing an AR tag detected in an image.
@@ -53,38 +21,13 @@ enum TagID
 class Tag
 {
 private:
-	/** Stores the corners of the tag in the image */
-	std::vector<cv::Point> corners;
-	cv::Vec3d rvec;
-	cv::Vec3d tvec;
-	TagID id;
-	CameraParams params;
-
-	/** Calculates the orientation */
-	void calcOrientation();
+	cv::Vec3d rvec_;
+	cv::Vec3d tvec_;
+	Marker marker_;
 
 public:
-	/**
-	   Constructor for a Tag. Takes four OpenCV points which are the coordinates of the
-	   corners of the Tag in the image. Note the order of points; this will be checked and
-	   an AR::InvalidCornerException will be thrown if ordering is incorrect.
-
-	   Also takes in a set of CameraParams, and the ID of this tag (the ID checking should be
-	   done outside of the Tag class).
-
-	   Please note that you should not need to call this constructor yourself as it is called
-	   by the Detector class, with values determined through image processing that happens in
-	   the Detector class.
-	*/
-	Tag(cv::Point top_left, cv::Point top_right, cv::Point bottom_right, cv::Point bottom_left,
-		CameraParams params, TagID tag_id = ID_UNKNOWN);
-	/** Gets the 2D (image) coordinates of the center of the tag. */
-	cv::Point getCenter() const;
-	/**
-	   Gets the 2D (image) corners of the tag. Note that this is a const operation and a copy
-	   of the corners will be returned.
-	*/
-	std::vector<cv::Point> getCorners() const;
+	Tag(Marker marker, cv::Vec3d rvec, cv::Vec3d tvec);
+	Tag() = delete;
 	/**
 	   Gets the rotation vector of the tag. This vector defines a line through the origin
 	   around which points are rotated to transform them from the object reference frame to the
@@ -112,9 +55,8 @@ public:
 	 */
 	cv::Vec3d getCoordinates() const;
 	/**
-	   Gets the ID of the tag. If the ID cannot be determined or is not a known ID marking a
-	   leg of the autonomous course, the ID_UNKNOWN value will be returned.
+	   Gets the Marker associated with the tag.
 	 */
-	TagID getID() const;
+	Marker getMarker() const;
 };
 } // namespace AR
