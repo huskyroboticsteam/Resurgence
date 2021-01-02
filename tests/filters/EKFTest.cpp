b8da81a3f2c14e18ede42df6bbcc6602ef8e3f03
Abhay D <a.deshpande012@gmail.com>
2021-01-02 12:57:53 -0800

    More ekf test case

--- tests/filters/EKFTest.cpp
+++ tests/filters/EKFTest.cpp
@@ -1,6 +1,3 @@
-#include <Eigen/Core>
-#include <iostream>
-
 #include <catch2/catch.hpp>
 
 #include "../../src/filters/ExtendedKalmanFilter.h"
@@ -28,16 +25,21 @@ double randWheelVel()
 {
 	return -5 + (rand() / (RAND_MAX / 10.0)); // random number between -5 and 5
 }
+
+pose_t randPose()
+{
+	return {randWheelVel(), randWheelVel(), randWheelVel()};
+}
 } // namespace
 
 TEST_CASE("EKF Predict-only")
 {
-	Eigen::Vector3d std = {0, 0, 0};
+	Eigen::Vector3d eps = {1e-3, 1e-3, 1e-3};
 	ExtendedKalmanFilter<3, 2, 3, 3, 3> ekf(
-		stateFunc, measurementFunc, NoiseCovMat<3, 3, 2>(std), NoiseCovMat<3, 3, 3>(std), 0.1);
+		stateFunc, measurementFunc, NoiseCovMat<3, 3, 2>(eps), NoiseCovMat<3, 3, 3>(eps), 0.1);
 
-	ekf.reset(std, std);
 	pose_t currPose = {0, 0, 0};
+	ekf.reset(currPose, eps);
 	for (int i = 0; i < 100; i++)
 	{
 		double l = randWheelVel();
@@ -45,10 +47,28 @@ TEST_CASE("EKF Predict-only")
 
 		constexpr int steps = 5; // maintain velocity for 0.5 sec
 		currPose = kinematics.getNextPose({l, r}, currPose, dt * steps);
-		for (int j = 0; j < steps; j++) {
+		for (int j = 0; j < steps; j++)
+		{
 			ekf.predict({l, r});
 		}
 
 		REQUIRE((currPose - ekf.getState()).norm() == Approx(0).margin(1e-9));
+	}
+}
+
+TEST_CASE("EKF Correct-only")
+{
+	Eigen::Vector3d eps = {1e-3, 1e-3, 1e-3};
+	ExtendedKalmanFilter<3, 2, 3, 3, 3> ekf(
+		stateFunc, measurementFunc, NoiseCovMat<3, 3, 2>(eps), NoiseCovMat<3, 3, 3>(eps), 0.1);
+
+	Eigen::Vector3d zero = {0, 0, 0};
+
+	for (int i = 0; i < 100; i++)
+	{
+		ekf.reset(zero);
+		pose_t pose = randPose();
+		ekf.correct(pose);
+		REQUIRE((ekf.getState() - pose).norm() == Approx(0).margin(1e-4));
 	}
 }
