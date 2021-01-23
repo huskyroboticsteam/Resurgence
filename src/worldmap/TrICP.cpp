849e4525204d2734c2522ec1661b691514759605
Abhay D <a.deshpande012@gmail.com>
2021-01-23 12:52:58 -0800

    Used kabsch algorithm for computing rigid transform

--- src/worldmap/TrICP.cpp
+++ src/worldmap/TrICP.cpp
@@ -1,6 +1,8 @@
 #include "TrICP.h"
 
-#include <Eigen/QR>
+#include <Eigen/LU>
+#include <Eigen/SVD>
+#include <iostream>
 
 void heapify(PointPair arr[], int len, int i)
 {
@@ -116,41 +118,48 @@ transform_t TrICP::computeTransformation(const std::vector<PointPair> &pairs)
 	 * the map. This transformation is restricted, as only rotations and translations are
 	 * permitted. Shearing and scaling is invalid.
 	 *
-	 * Instead of calculating the affine transformation directly, we'll calculate the
-	 * parameters directly. We have 3: translate x, translate y, rotate. This manifests as 4
-	 * variables: cosTheta, sinTheta, dx, dy. (can't directly calculate theta)
-	 *
-	 * This results in 2 equations per pair of points.
+	 * We can do this with the Kabsch algorithm.
+	 * https://en.wikipedia.org/wiki/Kabsch_algorithm
 	 */
-	size_t size = 2 * pairs.size();
-	Eigen::Matrix<double, Eigen::Dynamic, 4> A(size, 4);
-	Eigen::Matrix<double, Eigen::Dynamic, 1> B(size, 1);
-	for (int i = 0; i < pairs.size(); i++)
+	// TODO: Maybe transform the map to match the data sample?
+	Eigen::Vector2d mapCentroid = Eigen::Vector2d::Zero();
+	Eigen::Vector2d sampleCentroid = Eigen::Vector2d::Zero();
+
+	for (const PointPair &pair : pairs)
 	{
-		int index = 2 * i;
-		point_t from = pairs[i].samplePoint;
-		point_t to = pairs[i].mapPoint;
-		// x' = cosTheta * x - sinTheta * y + dx;
-		Eigen::RowVector4d row1 = {from(0), -from(1), 1, 0};
-		// y' = cosTheta * y + sinTheta * x + dy;
-		Eigen::RowVector4d row2 = {from(1), from(0), 0, 1};
-		A.row(index) = row1;
-		A.row(index + 1) = row2;
-		// first equation is for x', second is for y'
-		B.block<2, 1>(index, 0) = to.topRows<2>();
+		mapCentroid += pair.mapPoint.topRows<2>();
+		sampleCentroid += pair.samplePoint.topRows<2>();
 	}
-	// Least-squares solution. This vector is [cosTheta, sinTheta, dx, dy]
-	Eigen::Matrix<double, 4, 1> parameters = A.colPivHouseholderQr().solve(B);
-	double cosThetaApprox = parameters(0);
-	double sinThetaApprox = parameters(1);
-	double deltaX = parameters(2);
-	double deltaY = parameters(3);
-	// atan2 1) reconciles the different thetas in cosTheta and sinTheta (due to approximation)
-	// and 2) gets rid of any scale factors
-	double theta = atan2(sinThetaApprox, cosThetaApprox);
-	double cosTheta = cos(theta);
-	double sinTheta = sin(theta);
-	transform_t trf;
-	trf << cosTheta, -sinTheta, deltaX, sinTheta, cosTheta, deltaY, 0, 0, 1;
+
+	mapCentroid /= pairs.size();
+	sampleCentroid /= pairs.size();
+
+	int size = pairs.size();
+	Eigen::Matrix<double, Eigen::Dynamic, 2> P(size, 2);
+	Eigen::Matrix<double, Eigen::Dynamic, 2> Q(size, 2);
+	for (int i = 0; i < size; i++)
+	{
+		P.row(i) = pairs[i].samplePoint.topRows<2>() - sampleCentroid;
+		Q.row(i) = pairs[i].mapPoint.topRows<2>() - mapCentroid;
+	}
+
+	Eigen::Matrix2d H = P.transpose() * Q;
+
+	// computing SVD for square matrices, so no QR preconditioner needed
+	Eigen::JacobiSVD<Eigen::Matrix2d, Eigen::NoQRPreconditioner> svd;
+	svd.compute(H, Eigen::ComputeFullU | Eigen::ComputeFullV);
+
+	Eigen::Matrix2d U = svd.matrixU();
+	Eigen::Matrix2d V = svd.matrixV();
+	double d = (V * U.transpose()).determinant() > 0 ? 1 : -1;
+	Eigen::Matrix2d D = Eigen::Matrix2d::Identity();
+	D.bottomRightCorner<1, 1>()(0, 0) = d;
+	Eigen::Matrix2d R = V * D * U.transpose();
+
+	Eigen::Vector2d translation = mapCentroid - R * sampleCentroid;
+
+	transform_t trf = transform_t::Identity();
+	trf.topLeftCorner<2, 2>() = R;
+	trf.topRightCorner<2, 1>() = translation;
 	return trf;
 }
