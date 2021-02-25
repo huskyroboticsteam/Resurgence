49a97e39703e63b99432b084f9bf3f24c550b6d4
Abhay D <a.deshpande012@gmail.com>
2021-02-24 18:59:08 -0800

    Screw it linearquadtrees are too much work

--- src/worldmap/QuadTree.cpp
+++ src/worldmap/QuadTree.cpp
@@ -1,119 +1,129 @@
 #include "QuadTree.h"
 
-QuadTree::QuadTree(double width, int precision) : precision(precision), root(point_t(), width, precision)
+#include <algorithm>
+
+bool inBounds(const point_t &center, double size, const point_t &point)
+{
+	point_t diff = (point - center).array().abs();
+	double halfSize = size / 2;
+	return diff(0) <= halfSize && diff(1) <= halfSize;
+}
+
+QuadTree::QuadTree(point_t center, double width, int nodeCapacity)
+	: center(std::move(center)), width(width), points({}), nodeCapacity(nodeCapacity)
 {
 }
 
-void QuadTree::add(const point_t &point)
-{
-	root.add(point);
-}
-
-void QuadTree::remove(const point_t &point)
-{
-	root.remove(point);
-}
-
-point_t QuadTree::getClosest(const point_t &point)
-{
-	return root.getClosest(point);
-}
-
-std::vector<point_t> QuadTree::getPointsWithin(const point_t &point, double dist)
-{
-	return root.getPointsWithin(point, dist);
-}
-QuadTree::Node::Node(const point_t &center, double width, int levelsToLeaf)
-	: center(center), points(), width(width), levelsToLeaf(levelsToLeaf)
+QuadTree::QuadTree(double width, int nodeCapacity) : QuadTree({0, 0, 1}, width, nodeCapacity)
 {
 }
 
-//int getChildIndex(bool isTop, bool isRight) {
-//	return ((isTop ? 1 : 0) << 1) | (isRight ? 1 : 0);
-//}
-//
-//std::shared_ptr<Node> & getChildPtr(const Node &node, const point_t &point) {
-//	bool isRight = point(0) >= center(0);
-//	bool isTop = point(1) >= center(1);
-//	int index = ((isTop ? 1 : 0) << 1) | (isRight ? 1 : 0);
-//	return children[index];
-//}
-
-void QuadTree::Node::add(const point_t &point)
+bool QuadTree::add(const point_t &point)
 {
-	if (levelsToLeaf == 0) {
-		points.insert(point);
-	} else {
-		double d = width/4;
-		bool isRight = point(0) >= center(0);
-		bool isTop = point(1) >= center(1);
-		point_t newCenter = center + point_t(isRight ? d : -d, isTop ? d : -d, 1);
-		std::shared_ptr<Node> &ptr = getChildPtr(point);
-		if (!ptr) {
-			ptr.reset(new Node(newCenter, width/2, levelsToLeaf-1));
-		}
-		ptr->add(point);
+	if (!inBounds(center, width, point))
+	{
+		return false;
 	}
-}
-void QuadTree::Node::remove(const point_t &point)
-{
-	if (levelsToLeaf == 0) {
-		points.erase(point);
-	} else {
-		std::shared_ptr<Node> &ptr = getChildPtr(point);
-		if (ptr) {
-			ptr->remove(point);
+
+	if (points.size() < nodeCapacity && !hasChildren()) {
+		points.push_back(point);
+		return true;
+	}
+
+	if (!hasChildren()) {
+		subdivide();
+	}
+
+	for (const std::shared_ptr<QuadTree> &childPtr : children) {
+		if (childPtr->add(point)) {
+			return true;
 		}
 	}
+
+	// we should never get here
+	assert(false);
 }
-std::shared_ptr<QuadTree::Node> & QuadTree::Node::getChildPtr(const point_t &point)
+
+bool QuadTree::remove(const point_t &point)
 {
-	bool isRight = point(0) >= center(0);
-	bool isTop = point(1) >= center(1);
-	int index = ((isTop ? 1 : 0) << 1) | (isRight ? 1 : 0);
-	return children[index];
-}
-point_t QuadTree::Node::getClosest(const point_t &point)
-{
-	if (levelsToLeaf == 0) {
-		if (points.empty()) {
-			return {0,0,0};
-		} else {
-			point_t closest = {0,0,0};
-			double minDist = 1e9;
-			for (const point_t &p : points) {
-				double dist = (p - point).norm();
-				if (dist < minDist) {
-					closest = p;
-					minDist = dist;
-				}
+	if (!inBounds(center, width, point))
+	{
+		return false;
+	}
+
+	auto itr = std::find(points.begin(), points.end(), point);
+	if (itr != points.end()) {
+		points.erase(itr);
+		return true;
+	}
+
+	if (hasChildren()) {
+		for (const std::shared_ptr<QuadTree> &childPtr : children) {
+			if (childPtr->remove(point)) {
+				return true;
 			}
-			return closest;
-		}
-	} else {
-		std::shared_ptr<QuadTree::Node> &ptr = getChildPtr(point);
-		if (ptr) {
-			point_t closest = ptr->getClosest(point);
-			double minDist = (point - closest).norm();
-			if (levelsToLeaf == 1) {
-				for (const auto &childPtr : children) {
-					if (childPtr) {
-						point_t p = childPtr->getClosest(point);
-						double dist = (p - point).norm();
-						if (dist < minDist) {
-							minDist = dist;
-							closest = p;
-						}
-					}
-				}
-			}
-			return closest;
-		} else {
-			return {0,0,0};
 		}
 	}
+
+	return false;
 }
-points_t QuadTree::Node::getPointsWithin(const point_t &point, double dist)
+
+point_t QuadTree::getClosestWithin(const point_t &point, double size)
 {
-	return points_t();
+	points_t pointsInRange = getPointsWithin(point, size);
+	if (pointsInRange.empty()) {
+		return {0,0,0};
+	} else {
+		point_t closest = pointsInRange[0];
+		double minDist = (closest - point).norm();
+		for (int i = 1; i < pointsInRange.size(); i++) {
+			point_t p = pointsInRange[i];
+			double dist = (p - point).norm();
+			if (dist < minDist) {
+				minDist = dist;
+				closest = p;
+			}
+		}
+		return closest;
+	}
+}
+
+std::vector<point_t> QuadTree::getPointsWithin(const point_t &point, double size)
+{
+	points_t ret;
+	if (!inBounds(center, width, point))
+	{
+		return ret;
+	}
+
+	for (const point_t &p : points) {
+		if (inBounds(point, size, p)) {
+			ret.push_back(p);
+		}
+	}
+
+	if (hasChildren()) {
+		for (const auto &child : children) {
+			points_t pointsInChild = child->getPointsWithin(point, size);
+			ret.insert(ret.end(), pointsInChild.begin(), pointsInChild.end());
+		}
+	}
+
+	return ret;
+}
+
+void QuadTree::subdivide()
+{
+	double d = width/4;
+	for (int i = 0; i < 4; i++) {
+		double dx = (i & 1) == 0 ? -1 : 1;
+		double dy = (i & 0b10) == 0 ? -1 : 1;
+		point_t newCenter = center + d * point_t(dx,dy,0);
+		children[i] = std::make_shared<QuadTree>(center, width/2, nodeCapacity);
+	}
+}
+
+bool QuadTree::hasChildren()
+{
+	return static_cast<bool>(children[0]); // if one is initialized, all are
 }
--- src/worldmap/QuadTree.h
+++ src/worldmap/QuadTree.h
@@ -1,44 +1,27 @@
 #pragma once
 
 #include <memory>
-#include <unordered_set>
 
 #include "../simulator/utils.h"
 
 class QuadTree
 {
 public:
-	QuadTree(double width, int precision);
+	explicit QuadTree(double width, int nodeCapacity=16);
+	QuadTree(point_t center, double width, int nodeCapacity=16);
 
-	void add(const point_t &point);
-	void remove(const point_t &point);
-	point_t getClosest(const point_t &point);
-	points_t getPointsWithin(const point_t &point, double dist);
+	bool add(const point_t &point);
+	bool remove(const point_t &point);
+	point_t getClosestWithin(const point_t &point, double size);
+	points_t getPointsWithin(const point_t &point, double size);
 
 private:
-	struct PointHasher {
-		std::size_t operator()(const point_t &point) const {
-			auto hash = std::hash<double>();
-			return hash(point(0)) ^ hash(point(1));
-		}
-	};
+	std::shared_ptr<QuadTree> children[4];
+	point_t center;
+	double width;
+	points_t points;
+	int nodeCapacity;
 
-	struct Node {
-		Node(const point_t &center, double width, int levelsToLeaf);
-
-		void add(const point_t &point);
-		void remove(const point_t &point);
-		point_t getClosest(const point_t &point);
-		points_t getPointsWithin(const point_t &point, double dist);
-		std::shared_ptr<Node> & getChildPtr(const point_t &point);
-
-		std::shared_ptr<Node> children[4];
-		int levelsToLeaf;
-		point_t center;
-		double width;
-		std::unordered_set<point_t, PointHasher> points;
-	};
-
-	int precision;
-	Node root;
+	void subdivide();
+	bool hasChildren();
 };
