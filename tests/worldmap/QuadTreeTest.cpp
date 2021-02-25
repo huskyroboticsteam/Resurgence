42e6dbaa10b3b2a9358e2099349c06e3e624c190
Abhay D <a.deshpande012@gmail.com>
2021-02-25 13:28:35 -0800

    Added some more unit tests

--- tests/worldmap/QuadTreeTest.cpp
+++ tests/worldmap/QuadTreeTest.cpp
@@ -25,3 +25,43 @@ TEST_CASE("QuadTree - AddPoints", "[QuadTree]") {
 		REQUIRE(tree.getSize() == i+1);
 	}
 }
+
+TEST_CASE("QuadTree - RemovePoints", "[QuadTree]") {
+	srand(time(nullptr)); // NOLINT(cert-msc51-cpp)
+	QuadTree tree(100);
+
+	points_t points;
+
+	for (int i = 0; i < 50; i++) {
+		const point_t &point = randPoint(100);
+		points.push_back(point);
+		REQUIRE(tree.getSize() == i);
+		REQUIRE(tree.add(point));
+		REQUIRE(tree.getSize() == i+1);
+	}
+
+	int size = tree.getSize();
+	for (const point_t& point : points) {
+		REQUIRE(tree.getSize() == size);
+		REQUIRE(tree.remove(point));
+		REQUIRE(tree.getSize() == --size);
+	}
+	REQUIRE(tree.getSize() == 0);
+}
+
+TEST_CASE("QuadTree - RemoveNonexistentPoints", "[QuadTree]") {
+	srand(time(nullptr)); // NOLINT(cert-msc51-cpp)
+	QuadTree tree(100, 4);
+
+	for (int i = 0; i < 50; i++) {
+		// make the size smaller, so we can guarantee that some points are not in the tree
+		tree.add(randPoint(5));
+	}
+
+	REQUIRE_FALSE(tree.remove({53,1,1}));
+	REQUIRE_FALSE(tree.remove({0,0,0}));
+	REQUIRE_FALSE(tree.remove({-32,63,1}));
+	REQUIRE_FALSE(tree.remove({-32,-84,1}));
+	REQUIRE_FALSE(tree.remove({99,-13,1}));
+}
+
