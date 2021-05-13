0727fb744ee172be83ef5e522b5e2bee9ab70a5c
Husky <husky@jetson>
2021-05-12 19:34:30 -0700

    Don't send five simultaneous packets

--- src/Networking/FakeCANBoard.cpp
+++ src/Networking/FakeCANBoard.cpp
@@ -25,17 +25,17 @@ int main() {
   CANPacket p;
   uint8_t motor_group = 0x04;
   int test_type = prompt("What are you testing? (0 for MODE SET, 1 for PWM, 2 for PID)");
+  int serial = prompt("Enter motor serial");
 
   while(1) {
     if (test_type == TEST_MODE_SET) {
-      int serial = prompt("Enter motor serial");
+      serial = prompt("Enter motor serial");
       int mode = prompt("Enter mode (0 for PWM, 1 for PID)");
       std::cout << "got " << serial << " and " << mode << std::endl;
       AssembleModeSetPacket(&p, motor_group, (uint8_t) serial, (uint8_t) mode);
       sendCANPacket(p);
     }
     if (test_type == TEST_PWM) {
-      int serial = prompt("Enter motor serial");
       int pwm = prompt("Enter PWM");
       AssembleModeSetPacket(&p, motor_group, (uint8_t) serial, (uint8_t) 0x0);
       sendCANPacket(p);
@@ -43,24 +43,25 @@ int main() {
       sendCANPacket(p);
     }
     if (test_type == TEST_PID) {
-      int serial = prompt("Enter motor serial");
+      // Don't send all five packets at once. On some motor boards, the CAN buffer
+      // only fits four packets.
 
-      int p_coeff = prompt("P");
-      int i_coeff = prompt("I");
-      int d_coeff = prompt("D");
-
-      int angle_target = prompt("Enter PID target (in 1000ths of degrees)");
-
-      CANPacket p;
       AssembleModeSetPacket(&p, motor_group, (uint8_t) serial, (uint8_t) 0x1);
       sendCANPacket(p);
+
+      int p_coeff = prompt("P");
       AssemblePSetPacket(&p, motor_group, (uint8_t) serial, p_coeff);
       sendCANPacket(p);
+
+      int i_coeff = prompt("I");
       AssembleISetPacket(&p, motor_group, (uint8_t) serial, i_coeff);
       sendCANPacket(p);
+
+      int d_coeff = prompt("D");
       AssembleDSetPacket(&p, motor_group, (uint8_t) serial, d_coeff);
       sendCANPacket(p);
 
+      int angle_target = prompt("Enter PID target (in 1000ths of degrees)");
       AssemblePIDTargetSetPacket(&p, motor_group, (uint8_t) serial, angle_target);
       sendCANPacket(p);
     }
