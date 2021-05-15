344d05eb7703b8eb84dad7b3d848b6e4413d72ef
Abhay D <a.deshpande012@gmail.com>
2021-05-14 18:02:56 -0700

    Use collision predicate for alternate collision detection algorithms

--- src/planning/plan.h
+++ src/planning/plan.h
@@ -10,6 +10,7 @@
 using plan_t = Eigen::Matrix<double, Eigen::Dynamic, 2>;
 using action_t = Eigen::Vector2d;
 
+plan_t getPlan(const std::function<bool(double x, double y, double radius)> &, const point_t &, double);
 plan_t getPlan(const points_t &lidar_scan, const point_t &goal, double goal_radius);
 double planCostFromIndex(plan_t &plan, int idx);
 
