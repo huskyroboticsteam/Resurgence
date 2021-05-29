a8d62bf2bea763ed9f34eb8bb50a8966381a27e0
Abhay D <a.deshpande012@gmail.com>
2021-05-29 12:49:10 -0700

    Minor fix

--- src/commands/nogps/DriveToGate.cpp
+++ src/commands/nogps/DriveToGate.cpp
@@ -10,10 +10,16 @@ command_t DriveToGate::getOutput()
 	if (post(2) != 1)
 	{
 		return {0, slowVel};
-	} else {
+	}
+	else if (!isDone())
+	{
 		double angle = atan2(post.y(), post.x());
-		double vel = (post.topRows<2>().norm() <= 2 * radius) ? fastVel : slowVel;
+		double vel = (post.topRows<2>().norm() <= 2 * radius) ? slowVel : fastVel;
 		return {angle * thetaKP, vel};
+	}
+	else
+	{
+		return {0, 0};
 	}
 }
 
