868614842e811e5ae15cea497683f10ec82c76a7
Evan Straw <evan.straw99@gmail.com>
2021-05-31 13:56:43 -0700

    Move lidar reading into separate thread (#108)
    * Minor bug/warning fixes in various lidar-related files
    
    * Change line ending encoding
    
    * Read lidar in separate thread
    
    * Add timing for readLandmarks
    
    I don't really think it's necessary to do AR stuff in a separate
    thread - ScopedTimer reports max 1µs (and on average 0µs) for
    readLandmarks so it's probably not an issue. The ARUco code seems to
    be pretty efficient.
    
    * Small fix in GPS code
    
    While adding a "fresh data" functionality to the lidar code, I
    realized that fresh_data in the GPS code doesn't seem to ever be set
    to false - so it will always report that it has fresh data, even if it
    doesn't.
    
    * Refactor lidar initialization code
    
    * Refactor readLandmarks()
    
    Moved readLandmarks() implementation into a separate file and
    implemented threading for landmark detection.
    
    Co-authored-by: Nathan Hatch <nathanhatch297@gmail.com>

--- src/lidar/read_hokuyo_lidar.cpp
+++ src/lidar/read_hokuyo_lidar.cpp
@@ -2,48 +2,82 @@
 #include "PointCloudProcessing.h"
 #include "PointGenerator.h"
 #include "URGLidar.h"
+#include <atomic>
+#include <thread>
+#include <mutex>
 #include "../simulator/world_interface.h"
 #include "../simulator/utils.h"
+#include "read_hokuyo_lidar.h"
 
 // This website may be helpful
 // https://sourceforge.net/p/urgnetwork/wiki/top_en/
 
-bool lidar_initialized = false;
+constexpr double LIDAR_MIN_RANGE = 0.015;
+
+std::atomic<bool> lidar_initialized(false);
+std::atomic<bool> lidar_fresh(false);
 URGLidar urg_lidar;
+std::thread lidar_thread;
+points_t last_points = {};
+std::mutex points_lock;
 
-points_t readLidarScan() {
-  if (!lidar_initialized) {
-    if (!urg_lidar.open())
-    {
-      perror("failed to open lidar");
-      return {};
-    }
-    else
-    {
-      lidar_initialized = true;
-    }
-  }
+namespace lidar {
 
-  if (!urg_lidar.createFrame())
-  {
-    perror("failed to create frame");
-    return {};
-  }
+void readLidarLoop(){
+	while(true){
+		if (!urg_lidar.createFrame())
+		{
+			perror("failed to create frame");
+			continue;
+		}
 
-  std::vector<Polar2D> polarPts = urg_lidar.getLastFrame();
-  std::vector<point_t> pts(polarPts.size());
-  point_t origin({0,0,1});
-  for (int i = 0; i < polarPts.size(); i++)
-  {
-    pts[i] = lidar::polarToCartesian2(polarPts[i]);
-    if ((pts[i] - origin).norm() < 0.015)
-    {
-      // We're inside lidar min range, so this garbage data.
-      // We don't want to include this hit because otherwise the robot
-      // will think it's in collision with an obstacle.
-      pts[i](2) = 0.0;
-    }
-  }
-
-  return pts;
+		std::vector<Polar2D> polarPts = urg_lidar.getLastFrame();
+		std::vector<point_t> pts(polarPts.size());
+		point_t origin({0,0,1});
+		for (int i = 0; i < polarPts.size(); i++)
+		{
+			pts[i] = lidar::polarToCartesian2(polarPts[i]);
+			if ((pts[i] - origin).norm() < LIDAR_MIN_RANGE)
+			{
+				// We're inside lidar min range, so this is garbage data.
+				// We don't want to include this hit because otherwise the robot
+				// will think it's in collision with an obstacle.
+				pts[i](2) = 0.0;
+			}
+		}
+		lidar_fresh = true;
+		points_lock.lock();
+		last_points = pts;
+		points_lock.unlock();
+	}	
 }
+
+bool isLidarDataFresh(){
+	return lidar_fresh;
+}
+
+bool initializeLidar() {
+	if(!lidar_initialized) {
+		if (!urg_lidar.open()) {
+			perror("failed to open lidar");
+		} else {
+			lidar_thread = std::thread(&readLidarLoop);
+			lidar_initialized = true;
+		}
+	}
+	return lidar_initialized;
+}
+
+points_t readLidar() {
+	if (lidar_initialized) {
+		points_t pts;
+		points_lock.lock();
+		pts = last_points;
+		points_lock.unlock();
+		lidar_fresh = false;
+		return pts;
+	} else {
+		return {};
+	}
+}
+} // namespace lidar
