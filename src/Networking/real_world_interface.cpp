53f13df8984de91086392fd4d04b01f2fb35664b
Evan Straw <evan.straw99@gmail.com>
2021-05-31 00:41:20 -0700

    Refactor readLandmarks()
    Moved readLandmarks() implementation into a separate file and
    implemented threading for landmark detection.

--- src/Networking/real_world_interface.cpp
+++ src/Networking/real_world_interface.cpp
@@ -1,57 +1,34 @@
+#include <future>
 #include <iostream>
+#include <set>
+
+#include <opencv2/calib3d.hpp>
 
 #include "../Globals.h"
+#include "../Util.h"
+#include "../ar/read_landmarks.h"
 #include "../camera/Camera.h"
-#include "../ar/Detector.h"
+#include "../gps/read_usb_gps.h"
+#include "../lidar/read_hokuyo_lidar.h"
 #include "../log.h"
 #include "../simulator/utils.h"
 #include "../simulator/world_interface.h"
-#include "../gps/read_usb_gps.h"
-#include "json.hpp"
-#include "motor_interface.h"
 #include "CANUtils.h"
 #include "motor_interface.h"
-#include "../lidar/read_hokuyo_lidar.h"
-#include <set>
-#include <opencv2/calib3d.hpp>
-#include "../Util.h"
 
 #include "json.hpp"
 
-extern "C" {
+extern "C"
+{
 #include "../HindsightCAN/CANMotorUnit.h"
 }
 
 using nlohmann::json;
 
-cam::Camera ar_cam;
-AR::Detector ar_detector(AR::Markers::URC_MARKERS(), cam::CameraParams());
-points_t zero_landmarks;
-const point_t ZERO_POINT = {0.0, 0.0, 0.0};
-constexpr size_t NUM_LANDMARKS = 11;
-
 void world_interface_init() {
-	for(int i = 0; i < NUM_LANDMARKS; i++){
-		zero_landmarks.push_back({0.0, 0.0, 0.0});
-	}
-	try {
-		ar_cam.openFromConfigFile(Constants::AR_CAMERA_CONFIG_PATH);
-		if(!ar_cam.hasIntrinsicParams()){
-			log(LOG_ERROR, "Camera does not have intrinsic parameters! AR tag detection "
-						   "cannot be performed.\n");
-		} else {
-			ar_detector =
-				AR::Detector(AR::Markers::URC_MARKERS(), ar_cam.getIntrinsicParams());
-		}
-		if(!ar_cam.hasExtrinsicParams()){
-			log(LOG_WARN, "Camera does not have extrinsic parameters! Coordinates returned "
-						  "for AR tags will be relative to camera\n");
-		}
-	} catch (std::exception& e) {
-		log(LOG_ERROR, "Error opening camera for AR tag detection:\n%s\n", e.what());
-	}
 	bool gps_success = startGPSThread();
 	bool lidar_success = lidar::initializeLidar();
+	bool landmark_success = AR::initializeLandmarkDetection();
 }
 
 constexpr double WHEEL_BASE = 1.0;			   // Distance between left and right wheels. Eyeballed
@@ -96,66 +73,8 @@ bool setCmdVel(double dtheta, double dx) {
 	return true;
 }
 
-// Everything below this line still needs to be implemented
-points_t last_landmarks = zero_landmarks;
-uint32_t last_frame_no = 0;
-cv::Mat frame;
-points_t readLandmarks() {
-	// uncomment for timing of readLandmarks
-	// util::ScopedTimer("readLandmarks");
-
-	// camera might not be open if opening camera failed in init_world_interface.
-	// if it is not, return a list of 11 zero points.
-	if (ar_cam.isOpen()) {
-		// if we have a next frame, retrieve it and perform AR tag detection.
-		if (ar_cam.hasNext(last_frame_no)) {
-			ar_cam.next(frame, last_frame_no);
-			std::vector<AR::Tag> tags = ar_detector.detectTags(frame);
-			log(LOG_DEBUG, "readLandmarks(): %d tags spotted\n", tags.size());
-
-			// build up map with first tag of each ID spotted.
-			points_t output(NUM_LANDMARKS);
-			std::map<int, cv::Vec3d> ids_to_camera_coords;
-			for (AR::Tag tag : tags) {
-				int id = tag.getMarker().getId();
-				if (ids_to_camera_coords.find(id) == ids_to_camera_coords.end()) {
-					ids_to_camera_coords[id] = tag.getCoordinates();
-				}
-			}
-
-			// for every possible landmark ID, go through and if the map contains a value for
-			// that ID, add it to the output array (doing appropriate coordinate space
-			// transforms). If not, add a zero point.
-			for (int i = 0; i < NUM_LANDMARKS; i++) {
-				if (ids_to_camera_coords.find(i) != ids_to_camera_coords.end()) {
-					cv::Vec3d coords = ids_to_camera_coords[i];
-					// if we have extrinsic parameters, multiply coordinates by them to do
-					// appropriate transformation.
-					if (ar_cam.hasExtrinsicParams()) {
-						cv::Vec4d coords_homogeneous = {coords[0], coords[1], coords[2], 1};
-						cv::Mat transformed =
-							ar_cam.getExtrinsicParams() * cv::Mat(coords_homogeneous);
-						output[i] = {transformed.at<double>(0, 0),
-									 transformed.at<double>(1, 0), 1.0};
-					} else {
-						// just account for coordinate axis change; rover frame has +x front,
-						// +y left, +z up while camera has +x right, +y down, +z front.
-						output[i] = {coords[2], -coords[0], 1.0};
-					}
-				} else {
-					output[i] = ZERO_POINT;
-				}
-			}
-
-			// return output array.
-			last_landmarks = output;
-			return output;
-		} else {
-			return last_landmarks;
-		}
-	} else {
-		return zero_landmarks;
-	}
+points_t readLandmarks(){
+	return AR::readLandmarks();
 }
 
 points_t readLidarScan(){
