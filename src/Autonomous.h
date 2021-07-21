ea3cc60dbcd71fc54d898bbce6c319a7aa53a775
Nathan Hatch <nathanhatch297@gmail.com>
2021-07-21 15:47:38 -0600

    Implement using pose graph optimization to estimate landmark locations
    Previously we used a filter, which essentially took a moving average of
    the location estimates over time. However, now that the pose graph
    solves may suddenly shift the robot's estimated location, just taking
    the average of the location estimates is not a good idea.
    
    Fortunately, the pose graph itself can solve for landmark locations if
    we give it the data. This commit implements that.

--- src/Autonomous.h
+++ src/Autonomous.h
@@ -57,17 +57,16 @@ public:
 
 private:
 	std::vector<URCLeg> urc_targets;
-  size_t leg_idx; // which of the urc_targets we're currently navigating toward
+	size_t leg_idx; // which of the urc_targets we're currently navigating toward
 	pose_t search_target;
 	// Gate targets are {NAN, NAN, NAN} if unset and {INF, INF, INF} if reached
 	// gate_targets.second(2) is NAN if targets have not been refined with more accurate landmark measurements
 	std::pair<pose_t, pose_t> gate_targets;
+	bool gate_direction; // `true` if we go through the gate with left_post_id on our left
 	PoseEstimator poseEstimator;
 	GlobalMap map;
 	bool calibrated = false;
 	std::vector<pose_t> calibrationPoses{};
-	RollingAvgFilter<5,3> leftPostFilter;
-	RollingAvgFilter<5,3> rightPostFilter;
 	NavState nav_state;
 	ControlState control_state;
 	int time_since_plan;
@@ -87,7 +86,8 @@ private:
 	FriendlyGraph pose_graph;
 	int pose_id; // counter for how many poses we've added to the graph
 	transform_t prev_odom; // odom measurement at the time of the most recent pose in the graph
-	trajectory_t smoothed_traj; // cached solution to pose graph optimization
+	trajectory_t smoothed_traj; // cached solution to pose graph optimization (robot trajectory)
+	points_t smoothed_landmarks; // cached solution to pose graph optimization (AR tags)
 
 	rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr plan_pub;
 	rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr curr_pose_pub;
@@ -100,8 +100,10 @@ private:
 	void setNavState(NavState s);
 	void update_nav_state(const pose_t &pose, const pose_t &plan_target);
 	pose_t choose_plan_target(const pose_t &pose);
-	void updateLandmarkInformation(const transform_t &invTransform);
-	void computeGateTargets(const pose_t &pose);
+	int getPostID(bool left);
+	point_t getPostLocation(bool left);
+	bool getPostVisibility(bool left);
+	void computeGateTargets(const pose_t &pose, bool choose_direction);
 	void updateSearchTarget();
 	void endCurrentLeg();
 	transform_t optimizePoseGraph(transform_t current_gps, transform_t current_odom);
