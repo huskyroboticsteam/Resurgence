0b98e1854b924bd9e1f5ba0c4e99a8c1586b9f1d
Nathan Hatch <nathanhatch297@gmail.com>
2021-08-07 12:59:25 -0700

    Fix indentation

--- src/rospub.cpp
+++ src/rospub.cpp
@@ -13,16 +13,16 @@ namespace rospub {
 constexpr const char * ROVER_FRAME = "rover";
 
 std::map<PointPub, std::string> point_pub_names = {
-  {PointPub::PLAN_VIZ, "plan_viz"},
-  {PointPub::CURRENT_POSE, "current_pose"},
-  {PointPub::DRIVE_TARGET, "drive_target"},
-  {PointPub::PLAN_TARGET, "plan_target"},
-  {PointPub::POSE_GRAPH, "pose_graph"}
+	{PointPub::PLAN_VIZ, "plan_viz"},
+	{PointPub::CURRENT_POSE, "current_pose"},
+	{PointPub::DRIVE_TARGET, "drive_target"},
+	{PointPub::PLAN_TARGET, "plan_target"},
+	{PointPub::POSE_GRAPH, "pose_graph"}
 };
 
 std::map<ArrayPub, std::string> array_pub_names = {
-  {ArrayPub::LIDAR_SCAN, "lidar_scan"},
-  {ArrayPub::LANDMARKS, "landmarks"}
+	{ArrayPub::LIDAR_SCAN, "lidar_scan"},
+	{ArrayPub::LANDMARKS, "landmarks"}
 };
 
 rclcpp::Node *node;
@@ -31,27 +31,27 @@ std::map<PointPub, rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr> poin
 std::map<ArrayPub, rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr> array_pubs;
 
 void init() {
-  rclcpp::init(0, nullptr);
-  node = new rclcpp::Node("husky_pub");
+	rclcpp::init(0, nullptr);
+	node = new rclcpp::Node("husky_pub");
 
-  for (int idx = PointPub::PLAN_VIZ; idx <= PointPub::POSE_GRAPH; idx++)
-  {
-    PointPub idx_ = static_cast<PointPub>(idx);
-    point_pubs[idx_] = node->create_publisher<geometry_msgs::msg::Point>(
-        point_pub_names[idx_], 100);
-  }
+	for (int idx = PointPub::PLAN_VIZ; idx <= PointPub::POSE_GRAPH; idx++)
+	{
+		PointPub idx_ = static_cast<PointPub>(idx);
+		point_pubs[idx_] = node->create_publisher<geometry_msgs::msg::Point>(
+				point_pub_names[idx_], 100);
+	}
 
-  for (int idx = ArrayPub::LIDAR_SCAN; idx <= ArrayPub::LANDMARKS; idx++)
-  {
-    ArrayPub idx_ = static_cast<ArrayPub>(idx);
-    array_pubs[idx_] = node->create_publisher<geometry_msgs::msg::PoseArray>(
-        array_pub_names[idx_], 100);
-  }
+	for (int idx = ArrayPub::LIDAR_SCAN; idx <= ArrayPub::LANDMARKS; idx++)
+	{
+		ArrayPub idx_ = static_cast<ArrayPub>(idx);
+		array_pubs[idx_] = node->create_publisher<geometry_msgs::msg::PoseArray>(
+				array_pub_names[idx_], 100);
+	}
 }
 
 void shutdown() {
-  rclcpp::shutdown();
-  free(node);
+	rclcpp::shutdown();
+	free(node);
 }
 
 void publish(const pose_t &pose, PointPub topic)
@@ -60,7 +60,7 @@ void publish(const pose_t &pose, PointPub topic)
 	message.x = pose(0);
 	message.y = pose(1);
 	message.z = pose(2);
-  point_pubs[topic]->publish(message);
+	point_pubs[topic]->publish(message);
 }
 
 void publish_array(const points_t &points, ArrayPub topic)
