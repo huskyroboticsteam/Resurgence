ec15c1337653dcda1cf33935a56cd743259e81fa
Abhay D <a.deshpande012@gmail.com>
2022-02-02 23:10:58 -0800

    Added doxy comments

--- src/world_interface/kinematic_common_interface.h
+++ src/world_interface/kinematic_common_interface.h
@@ -2,7 +2,20 @@
 
 #include "../kinematics/DiffDriveKinematics.h"
 
+/**
+ * @brief Call this method from all implementations of setCmdVel().
+ * This is required, and will allow odometry methods to work correctly.
+ * This also keeps track of the velocity to return for getCmdVel().
+ *
+ * @param wheelVels The tangential wheel velocities of the left and right wheels.
+ */
 void setCmdVelToIntegrate(const wheelvel_t& wheelVels);
 
+/**
+ * @brief Call this method from all implementations of setCmdVel().
+ * This is required, and will allow odometry methods to work correctly.
+ * This also keeps track of the velocity to return for getCmdVel().
+ *
+ * @param wheelVels The heading velocity (rad/s) and forward velocity (m/s).
+ */
 void setCmdVelToIntegrate(double dtheta, double dx);
-
