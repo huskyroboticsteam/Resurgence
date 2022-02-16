16d62b57de45dad9904a0120ad6058f62720a345
John Li <30376878+johnli01@users.noreply.github.com>
2022-02-15 20:34:49 -0800

    Update ar tag (#151)
    * Updated ar tag markers to 2022 season
    
    * Change marker physical size
    
    Co-authored-by: Evan Straw <evan.straw99@gmail.com>

--- src/ar/MarkerSet.cpp
+++ src/ar/MarkerSet.cpp
@@ -105,185 +105,27 @@ bool MarkerSet::getMarkerByMappedID(int mapped_id, MarkerPattern& out) const {
 namespace Markers {
 
 // Constants - please note that these are only used internally.
-/** The number of ALVAR markers stored in the data array. */
-constexpr size_t ALVAR_COUNT = 18;
-/** The size in pixels of the data region of an ALVAR marker. */
-constexpr size_t ALVAR_BIT_SIZE = 5;
-/** The border size in pixels of one side of an ALVAR marker. */
-constexpr size_t ALVAR_BORDER_SIZE = 2;
-/** The physical size (in mm) of an ALVAR marker. */
-constexpr float ALVAR_PHYS_SIZE = 0.2;
-/** The bit array size for one ALVAR marker, equal to the bit size squared. */
-constexpr size_t BIT_ARR_SIZE = ALVAR_BIT_SIZE * ALVAR_BIT_SIZE;
-
 /** The size in pixels of the data region of an ARUco marker. */
 constexpr size_t ARUCO_BIT_SIZE = 4;
 /** The border size in pixels of one side of an ARUco marker. */
 constexpr size_t ARUCO_BORDER_SIZE = 1;
 /** The physical size (in mm) of an ARUco marker. */
-constexpr float ARUCO_PHYS_SIZE = 0.1; // we don't know this yet, change later
+constexpr float ARUCO_PHYS_SIZE = 0.15; // we don't know this yet, change later
 
-/**
-   2D array of the bits in the ALVAR markers, used in URC. Each marker is 5x5, not
-   including border, which is 2 bits wide. 0 is black, 1 is white. The markers can be
-   found here:
-
-   https://wiki.ros.org/ar_track_alvar?action=AttachFile&do=view&target=markers0to8.png
-   https://wiki.ros.org/ar_track_alvar?action=AttachFile&do=view&target=markers9to17.png
-
-   Note this array is only used internally.
- */
-uint8_t __alvar_markers[ALVAR_COUNT][BIT_ARR_SIZE] = {
-	// we have to turn the formatter off here because it's easier to tell the shape of the
-	// tags in this format.
-
-	// clang-format off
-
-	// ALVAR Marker 0
-	{1,1,0,1,1,
-	 1,1,0,1,1,
-	 1,0,1,0,1,
-	 1,1,1,1,1,
-	 1,1,1,1,1},
-	// ALVAR Marker 1
-	{1,1,0,1,1,
-	 1,1,0,1,1,
-	 1,0,1,0,1,
-	 0,0,1,1,0,
-	 1,1,1,0,1},
-	// ALVAR Marker 2
-	{1,1,0,1,1,
-	 1,1,0,1,1,
-	 1,0,1,0,1,
-	 1,0,1,1,0,
-	 1,0,1,1,0},
-	// ALVAR Marker 3
-	{1,1,0,1,1,
-	 1,1,0,1,1,
-	 1,0,1,0,1,
-	 0,1,1,1,1,
-	 1,0,1,0,0},
-	// ALVAR Marker 4
-	{1,1,0,1,1,
-	 1,1,0,1,1,
-	 1,0,1,0,1,
-	 0,1,1,1,0,
-	 0,1,1,1,0},
-	// ALVAR Marker 5
-	{1,1,0,1,1,
-	 1,1,0,1,1,
-	 1,0,1,0,1,
-	 1,0,1,1,1,
-	 0,1,1,0,0},
-	// ALVAR Marker 6
-	{1,1,0,1,1,
-	 1,1,0,1,1,
-	 1,0,1,0,1,
-	 0,0,1,1,1,
-	 0,0,1,1,1},
-	// ALVAR Marker 7
-	{1,1,0,1,1,
-	 1,1,0,1,1,
-	 1,0,1,0,1,
-	 1,1,1,1,0,
-	 0,0,1,0,1},
-	// ALVAR Marker 8
-	{1,1,0,1,1,
-	 1,1,0,1,1,
-	 1,0,1,0,1,
-	 0,0,1,0,1,
-	 1,1,1,1,0},
-	// ALVAR Marker 9
-	{1,1,0,1,1,
-  	 1,1,0,1,1,
-  	 1,0,1,0,1,
-  	 1,1,1,0,0,
-  	 1,1,1,0,0},
-	// ALVAR Marker 10
- 	{1,1,0,1,1,
-  	 1,1,0,1,1,
-  	 1,0,1,0,1,
-  	 0,1,1,0,0,
-  	 1,0,1,1,1},
-	// ALVAR Marker 11
- 	{1,1,0,1,1,
- 	 1,1,0,1,1,
- 	 1,0,1,0,1,
- 	 1,0,1,0,1,
- 	 1,0,1,0,1},
-	// ALVAR Marker 12
- 	{1,1,0,1,1,
- 	 1,1,0,1,1,
-  	 1,0,1,0,1,
- 	 1,0,1,0,0,
- 	 1,1,1,1,1},
-	// ALVAR Marker 13
- 	{1,1,0,1,1,
- 	 1,1,0,1,1,
- 	 1,0,1,0,1,
- 	 0,1,1,0,1,
- 	 0,1,1,0,1},
-	// ALVAR Marker 14
- 	{1,1,0,1,1,
- 	 1,1,0,1,1,
- 	 1,0,1,0,1,
- 	 1,1,1,0,1,
- 	 0,0,1,1,0},
-	// ALVAR Marker 15
- 	{1,1,0,1,1,
- 	 1,1,0,1,1,
- 	 1,0,1,0,1,
- 	 0,0,1,0,0,
- 	 0,0,1,0,0},
-	// ALVAR Marker 16
- 	{0,0,0,1,1,
- 	 1,1,0,0,1,
- 	 1,0,1,0,1,
- 	 1,1,1,1,1,
- 	 1,1,1,1,1},
-	// ALVAR Marker 17
- 	{0,0,0,1,1,
- 	 1,1,0,0,1,
- 	 1,0,1,0,1,
- 	 0,0,1,1,0,
- 	 1,1,1,0,1}
-
-	// clang-format on
-};
-
-/**
-   Constructs the "bytes list" (used by the ARUco module to construct dictionaries)
- */
-cv::Mat makeAlvarBytesList(uint8_t marker_array[ALVAR_COUNT][BIT_ARR_SIZE]) {
-	cv::Mat bytes_list;
-	cv::Size size(ALVAR_BIT_SIZE, ALVAR_BIT_SIZE);
-	for (size_t m = 0; m < ALVAR_COUNT; m++) {
-		uint8_t* current_marker = marker_array[m];
-		cv::Mat current_marker_mat = cv::Mat(size, CV_8UC1, current_marker);
-		bytes_list.push_back(cv::aruco::Dictionary::getByteListFromBits(current_marker_mat));
-	}
-	return bytes_list;
-}
 
 /**
    Constructs the URC marker set
  */
 MarkerSet makeURCSet() {
-	static const cv::aruco::Dictionary alvar_dict(makeAlvarBytesList(__alvar_markers),
-												  ALVAR_BIT_SIZE, 1);
-	MarkerSet set(ALVAR_BIT_SIZE, ALVAR_BORDER_SIZE, ALVAR_PHYS_SIZE, alvar_dict);
-
-	set.addIDMapping(0, LEG1);
-	set.addIDMapping(1, LEG2);
-	set.addIDMapping(2, LEG3);
-	set.addIDMapping(3, LEG4_L);
-	set.addIDMapping(4, LEG4_R);
-	set.addIDMapping(5, LEG5_L);
-	set.addIDMapping(6, LEG5_R);
-	set.addIDMapping(7, LEG6_L);
-	set.addIDMapping(8, LEG6_R);
-	set.addIDMapping(9, LEG7_L);
-	set.addIDMapping(10, LEG7_R);
+	static const cv::Ptr<cv::aruco::Dictionary> urc_dict_ptr =
+		cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+	MarkerSet set(ARUCO_BIT_SIZE, ARUCO_BORDER_SIZE, ARUCO_PHYS_SIZE, urc_dict_ptr);
+	set.addIDMapping(0, START);
+	set.addIDMapping(1, POST1);
+	set.addIDMapping(2, POST2);
+	set.addIDMapping(3, POST3);
+	set.addIDMapping(4, GATEL);
+	set.addIDMapping(5, GATER);
 
 	return set;
 }
