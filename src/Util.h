e5452b833c30a668a81db9b61b87aeac0234d4be
Abhay D <a.deshpande012@gmail.com>
2022-02-21 15:13:33 -0800

    Added documentation, removed unimplemented methods

--- src/Util.h
+++ src/Util.h
@@ -4,12 +4,23 @@
 
 #include <chrono>
 #include <string>
-#include <time.h>
 
 #include <Eigen/Geometry>
-#include <sys/time.h>
 
+/**
+ * @namespace util
+ * @brief A collection of utility functions and classes with common use-cases.
+ */
 namespace util {
+
+/**
+ * @brief Check if two numbers are approximately equal.
+ *
+ * @param a The first number.
+ * @param b The second number.
+ * @param threshold If @c |a-b|&ge;threshold then they are not approximately equal.
+ * @return true iff @c |a-b|<threshold.
+ */
 bool almostEqual(double a, double b, double threshold = 1e-6);
 
 /**
@@ -36,27 +47,35 @@ double quatToHeading(double qw, double qx, double qy, double qz);
 double quatToHeading(Eigen::Quaterniond quat);
 
 /**
- * A utility class that helps with timing. Measures the elapsed time from the moment
- * this class is instantiated to the moment that the scope ends.
+ * @brief A utility class that helps with timing.
+ *
+ * Measures the elapsed time from the moment this
+ * class is instantiated to the moment that it's destructed.
  */
 class ScopedTimer {
 public:
 	/**
-	 * Create a scoped timer with the specified name. The elapsed time will be printed
-	 * to stdout at the end of the scope.
+	 * @brief Create a scoped timer with the specified name.
+	 *
+	 * The elapsed time will be printed to stdout when destructed.
+	 *
 	 * @param name The name of this timer.
 	 */
 	ScopedTimer(std::string name);
 
 	/**
-	 * Create an unnamed scoped timer. Nothing will be printed by this timer.
+	 * @brief Create an unnamed scoped timer.
+	 *
+	 * Nothing will be printed by this timer.
 	 */
 	ScopedTimer();
+
 	~ScopedTimer();
 
 	/**
-	 * Gets the elapsed time measured by this scoped timer.
-	 * @return
+	 * @brief Gets the elapsed time measured by this timer.
+	 *
+	 * @return The elapsed time, in microseconds.
 	 */
 	std::chrono::microseconds elapsedTime() const;
 
@@ -65,55 +84,117 @@ private:
 	std::string name;
 };
 
+/**
+ * @brief Convert a set of points to a different reference frame.
+ *
+ * Applies @p tf^-1 to every element of @p ps and returns the transformed points.
+ *
+ * @param ps The points to convert between reference frames.
+ * @param tf The transform whose inverse will be applied to the points.
+ * @return Points such that the the i-th point is equivalent to @c tf^-1*ps[i].
+ *
+ * @note The inverse of the given transform is applied to every point,
+ * not the transform itself.
+ */
 navtypes::points_t transformReadings(const navtypes::points_t& ps,
 									 const navtypes::transform_t& tf);
+
+/**
+ * @brief Convert a trajectory to a different reference frame.
+ *
+ * Applies @p tf on the right for every element of @p traj and returns
+ * the transformed trajectory.
+ *
+ * @param traj The trajectory to transform.
+ * @param tf The transform to apply on the right to every element of @p traj.
+ * @return The transformed trajectory, such that the i-th element is @c traj[i]*tf.
+ */
 navtypes::trajectory_t transformTraj(const navtypes::trajectory_t& traj,
 									 const navtypes::transform_t& tf);
 
-/* Whether the robot in location given by `tf` collides with anything. */
-bool collides(const navtypes::transform_t& tf, const navtypes::obstacles_t& obss);
+/**
+ * @brief Check for intersection between a pose and a point cloud.
+ *
+ * Checks if any point of @p lms is within @p radius distance of the robot.
+ *
+ * @param tf The robot transform.
+ * @param lms The points to check against for intersection.
+ * @param radius The minimum distance from the robot to a
+ * point to not count as an intersection.
+ * @return If any point of @p lms is within @p radius distance of the robot.
+ */
 bool collides(const navtypes::transform_t& tf, const navtypes::points_t& lms, double radius);
 
-/* Returns the smallest number t in the unit interval [0,1] such that
- * the point r0 + t(r1-r0) lies on an obstacle boundary. If there is no such point,
- * returns t=2.0. */
-double obstacleIntersection(const navtypes::point_t& r0, const navtypes::point_t& r1,
-							const navtypes::obstacles_t& obss);
-
-/* Left-multiplying a (map-frame) point_t by this matrix will give the
- * corresponding point_t in the frame that was rotated counterclockwise
- * by theta, then shifted by (x,y) along the new (x,y)-axes.
+/**
+ * @brief Creates a rigid transform that takes a point from the map-frame
+ * to a frame that was rotated CCW by theta and shifted (x,y) along the new axes.
  *
- * For example, for (x, y, theta) = (1, 0, pi/2), the origin frame
+ * This can also be viewed as rotating a map-frame point CW by theta and
+ * then shifting by (-x,-y) along the map axes.
  *
- *   . .
- *   > .
- *
- * becomes
- *
- *   ^ .
- *   . .
+ * @param x The x-coordinate in map-space of the origin of the new frame.
+ * @param y The y-coordinate in map-space of the origin of the new frame.
+ * @param theta The CCW angle between the new frame and the old frame.
+ * @return A rigid transform that changes frames as described.
  */
 navtypes::transform_t toTransformRotateFirst(double x, double y, double theta);
 
-// Given a transform, gives the robot pose that would have that transform.
-// That is, the x,y location and angle theta of the robot such that left-multiplying a
-// world-frame landmark location by `trf` will give you the robot-frame landmark location.
-//
-// NOTE: THIS IS AN INVERSE OF toTransform, NOT toTransformRotateFirst.
-// The parameters of toTransformRotateFirst are not a robot pose
-// (rotation and translation operations do not commute).
+/**
+ * @brief Given a transform, gives a robot pose that would have that transform.
+ *
+ * That is, the x,y location and angle theta of the robot such that left-multiplying
+ * a map-frame coordinate by `trf` will give you the robot-frame coordinate.
+ *
+ * @param trf The rigid transform representing a robot position.
+ * This transform would take points from the map frame to the robot frame.
+ * @param prev_theta Since heading is modular, the returned heading value will be
+ * chosen to be within 2&pi; radians of this parameter.
+ * @return The robot pose.
+ *
+ * @warning THIS IS AN INVERSE OF util::toTransform, NOT util::toTransformRotateFirst.
+ * The parameters of the latter are not a robot pose
+ * (rotation and translation operations do not commute).
+ */
 navtypes::pose_t toPose(const navtypes::transform_t& trf, double prev_theta);
-double nearestHeadingBranch(double theta, double prev_theta);
 
+/**
+ * @brief Get an equivalent heading that is within &pi; radians of another heading.
+ *
+ * @param theta The heading to get the equivalent heading for.
+ * @param prev_theta The heading that the returned heading is in the neighborhood of.
+ * @return A heading equivalent to @p theta that is within &pi; radians of @p prev_theta.
+ */
+double closestHeading(double theta, double prev_theta);
+
+/**
+ * @brief Convert a pose to a rigid transform.
+ *
+ * Calculate the rigid transform associated with the given pose.
+ * This rigid transform takes points from the global frame to
+ * the robot frame.
+ *
+ * @param pose The current robot pose.
+ * @return navtypes::transform_t The rigid transform denoting the pose of the robot.
+ */
 navtypes::transform_t toTransform(const navtypes::pose_t& pose);
 
-// One sample from a standard normal distribution.
-// The thread_id is used to choose which random number generator to use.
-// This is important when trying to rerun a particular random seed; each thread
-// needs its own dedicated sequence of random numbers.
+/**
+ * @brief Sample from the standard normal distribution.
+ *
+ * The thread_id is used to choose which random number generator to use.
+ * This is important when trying to rerun a particular random seed;
+ * each thread needs its own dedicated sequence of random numbers.
+ *
+ * @param thread_id Either 0 or 1, denoting which random number generator use.
+ * @return double
+ */
 double stdn(int thread_id);
-// Returns the random seed used for the stdn() function
+
+/**
+ * @brief Get the random seed used for util::stdn().
+ *
+ * @return The random seed.
+ */
 long getNormalSeed();
 
 } // namespace util
