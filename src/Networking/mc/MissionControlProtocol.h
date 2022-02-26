156afcbe069823ba7e6e5065a86f5a6e955e10ef
Evan Straw <evan.straw99@gmail.com>
2022-02-26 13:32:47 -0800

    Add MissionControlProtocol class

--- src/Networking/mc/MissionControlProtocol.h
+++ src/Networking/mc/MissionControlProtocol.h
@@ -1,23 +1,45 @@
 #pragma once
 
-#include "../websocket/WebSocketProtocol.h"
 #include "../../world_interface/world_interface.h"
+#include "../websocket/WebSocketProtocol.h"
+#include "../websocket/WebSocketServer.h"
+
+#include <atomic>
+#include <memory>
 #include <unordered_set>
 
 namespace mc {
 using json = nlohmann::json;
+using websocket::SingleClientWSServer;
+using websocket::WebSocketProtocol;
 
-/**
-   @brief Initialize the mission control protocol.
+class MissionControlProtocol : WebSocketProtocol {
+public:
+	MissionControlProtocol(SingleClientWSServer& server);
+	~MissionControlProtocol();
+	std::unordered_set<CameraID> getOpenCameraStreams();
 
-   @return A websocket::WebSocketProtocol object representing the protocol, which can be passed
-   to SingleClientWSServer::addProtocol(), and contains the appropriate message
-   handlers/validators.
- */
-websocket::WebSocketProtocol initMissionControlProtocol();
-/**
-   @brief Get the set of open camera streams as a set of camera IDs.
- */
-std::unordered_set<CameraID> getOpenCameraStreams();
-
+private:
+	void videoStreamTask();
+	std::atomic<bool> _streaming_running;
+	SingleClientWSServer& _server;
+	std::unordered_set<CameraID> _open_streams;
+	////// VALIDATORS //////
+	bool validateEmergencyStopRequest(const json& j);
+	bool validateOperationModeRequest(const json& j);
+	bool validateDriveRequest(const json& j);
+	bool validateMotorPowerRequest(const json& j);
+	bool validateMotorPositionRequest(const json& j);
+	bool validateCameraStreamOpenRequest(const json& j);
+	bool validateCameraStreamCloseRequest(const json& j);
+	////// HANDLERS //////
+	void handleEmergencyStopRequest(const json& j);
+	void handleOperationModeRequest(const json& j);
+	void handleDriveRequest(const json& j);
+	void handleMotorPowerRequest(const json& j);
+	void handleMotorPositionRequest(const json& j);
+	void handleCameraStreamOpenRequest(const json& j);
+	void handleCameraStreamCloseRequest(const json& j);
 };
+
+}; // namespace mc
