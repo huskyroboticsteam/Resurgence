ef1f08cfb3cced19a4bc51b022d8add2d9a059f2
Abhay D <a.deshpande012@gmail.com>
2022-04-18 17:21:00 -0700

    Added stuff to CANStubs

--- src/CAN/CANStubs.cpp
+++ src/CAN/CANStubs.cpp
@@ -1,15 +1,27 @@
 
 #include "CAN.h"
+
 #include <iostream>
 
 namespace can {
+namespace {
+uint32_t callbackID = 0;
+};
+
 void initCAN() {}
 
-void sendCANPacket(const CANPacket& packet) {
-	std::cout << "Packet: " << packetToString(packet) << std::endl;
-}
+void sendCANPacket(const CANPacket& packet) {}
 
 robot::types::DataPoint<telemetry_t> getDeviceTelemetry(deviceid_t id, telemtype_t telemType) {
 	return {};
 }
+
+callbackid_t addDeviceTelemetryCallback(
+	deviceid_t id, telemtype_t telemType,
+	const std::function<void(deviceid_t, telemtype_t, robot::types::DataPoint<telemetry_t>)>&
+		callback) {
+	return {id, telemType, callbackID++};
+}
+
+void removeDeviceTelemetryCallback(callbackid_t id) {}
 } // namespace can
