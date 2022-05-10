3eaa71a5abd459020e3133bc39055258d007b2bb
Abhay D <a.deshpande012@gmail.com>
2022-05-09 18:11:02 -0700

    Refactor tests

--- tests/filters/MultiSensorEKFTest.cpp
+++ tests/filters/MultiSensorEKFTest.cpp
@@ -1,41 +1,63 @@
-#include <catch2/catch.hpp>
-
 #include <stdexcept>
-#define eigen_assert(x) if (!(x)) { throw std::runtime_error(#x); }
+
+#include <catch2/catch.hpp>
+#define eigen_assert(x)                                                                       \
+	if (!(x)) {                                                                               \
+		throw std::runtime_error(#x);                                                         \
+	}
 
 #include "../../src/filters/MultiSensorEKF.h"
 
 #include <Eigen/Core>
 
-#include <iostream>
-
 using namespace filters;
 using namespace filters::statespace;
 
-constexpr int stateDim = 3;
-constexpr int inputDim = 2;
+TEST_CASE("Test Output Jacobian", "[filters]") {
+	auto outputFunc = [](const Eigen::VectorXd& x, const Eigen::VectorXd& b) {
+		return (2 * x + b).topRows(x.size()-1);
+	};
 
-Eigen::VectorXd outputFunc1(const Eigen::VectorXd& x, const Eigen::VectorXd& b) {
-	return (2 * x + b.topRows(stateDim)).topRows(x.size()-1);
+	Eigen::VectorXd std(Eigen::VectorXd::Ones(3));
+	filters::statespace::NoiseCovMatX cov(std, 3, 2);
+	filters::Output o(3, 2, 3, outputFunc, cov);
+
+	Eigen::VectorXd state(Eigen::VectorXd::Ones(3));
+
+	Eigen::MatrixXd jacobX = o.outputFuncJacobianX(state);
+	Eigen::MatrixXd jacobV = o.outputFuncJacobianV(state);
+
+	Eigen::MatrixXd jacobXTrue(2,3);
+	jacobXTrue << 2, 0, 0,
+				  0, 2, 0;
+
+	REQUIRE((jacobX - jacobXTrue).array().abs().maxCoeff() <= 1e-4);
+	REQUIRE((jacobV - jacobXTrue/2).array().abs().maxCoeff() <= 1e-4);
 }
 
-Eigen::VectorXd outputFunc2(const Eigen::VectorXd& x, const Eigen::VectorXd& b) {
-	return Eigen::Vector3d::Ones() * ((2 * x + b).norm());
-}
+TEST_CASE("MultiSensorEKF Test Runtime Errors", "[filters]") {
+	constexpr int stateDim = 3;
+	constexpr int inputDim = 2;
 
-Eigen::Matrix<double, stateDim, 1> stateFunc(const Vectord<stateDim>& x, const Vectord<inputDim>& u, const Vectord<stateDim>& noise) {
-	return x + noise;
-}
+	auto outputFunc1 = [](const Eigen::VectorXd& x, const Eigen::VectorXd& b) {
+		return (2 * x + b.topRows(stateDim)).topRows(x.size() - 1);
+	};
+	auto outputFunc2 = [](const Eigen::VectorXd& x, const Eigen::VectorXd& b) {
+		return Eigen::Vector3d::Ones() * ((2 * x + b).norm());
+	};
+	auto stateFunc = [](const Vectord<stateDim>& x, const Vectord<inputDim>& u,
+						const Vectord<stateDim>& noise) { return x + noise; };
 
-TEST_CASE("MultiSensorEKFTest") {
 	NoiseCovMatX covMat1(Eigen::MatrixXd::Identity(4, 4).eval(), stateDim, 2);
 	Output out1(stateDim, 2, 4, outputFunc1, covMat1);
 	NoiseCovMatX covMat2(Eigen::MatrixXd::Identity(3, 3).eval(), stateDim, 3);
 	Output out2(stateDim, 3, 3, outputFunc2, covMat2);
-	NoiseCovMat<stateDim, stateDim, inputDim> processNoise(Eigen::Matrix<double, stateDim, stateDim>::Identity().eval());
+	NoiseCovMat<stateDim, stateDim, inputDim> processNoise(
+		Eigen::Matrix<double, stateDim, stateDim>::Identity().eval());
 	std::array<Output, 2> outputs = {out1, out2};
-	filters::MultiSensorEKF<stateDim, inputDim, stateDim, 2> ekf(stateFunc, processNoise, 0.1, outputs);
+	filters::MultiSensorEKF<stateDim, inputDim, stateDim, 2> ekf(stateFunc, processNoise, 0.1,
+																 outputs);
 	REQUIRE_NOTHROW(ekf.predict(Eigen::Vector2d::Zero().eval()));
 	REQUIRE_NOTHROW(ekf.correct<0>(Eigen::Vector2d::Ones().eval()));
-	ekf.correct<1>(Eigen::Vector3d::Ones().eval() * 3);
+	REQUIRE_NOTHROW(ekf.correct<1>(Eigen::Vector3d::Ones().eval() * 3));
 }
--- tests/filters/StateSpaceUtilsTest.cpp
+++ tests/filters/StateSpaceUtilsTest.cpp
@@ -4,7 +4,7 @@
 
 using namespace filters;
 
-TEST_CASE("StateSpace - Continuous to Discrete System Model") {
+TEST_CASE("Test Continuous to Discrete System Model", "[statespace][filters]") {
 	// examples pulled from appendix of https://doi.org/10.1016/0307-904X(80)90177-8
 	Eigen::Matrix2d A, B;
 	A << 1, 2, 3, -4;
@@ -20,7 +20,7 @@ TEST_CASE("StateSpace - Continuous to Discrete System Model") {
 	CHECK((B-discB).cwiseAbs().sum() == Approx(0).margin(1e-4));
 }
 
-TEST_CASE("StateSpace - Discrete to Continuous System Model") {
+TEST_CASE("Test Discrete to Continuous System Model", "[statespace][filters]") {
 	Eigen::Matrix2d A, B;
 	A << 1.45412, 0.38920, 0.58381, 0.48111;
 	B << 0.64872, 0.05190, 0.32437, 0.16865;
@@ -35,15 +35,15 @@ TEST_CASE("StateSpace - Discrete to Continuous System Model") {
 	CHECK((B-contB).cwiseAbs().sum() == Approx(0).margin(1e-4));
 }
 
-TEST_CASE("Test Numerical Jacobian") {
-	{
+TEST_CASE("Test Numerical Jacobian", "[statespace][filters]") {
+	SECTION("Test identity function") {
 		auto func = [](const Eigen::Vector3d& x) { return x; };
 		Eigen::MatrixXd jacob = statespace::numericalJacobian(func, Eigen::Vector3d::Random(), 3);
 		REQUIRE(jacob.rows() == 3);
 		REQUIRE(jacob.cols() == 3);
 		REQUIRE((jacob - Eigen::Matrix3d::Identity()).cwiseAbs().maxCoeff() <= 1e-4);
 	}
-	{
+	SECTION("Test inner product with self") {
 		auto func = [](const Eigen::Vector3d& x) { return x.transpose() * x; };
 		Eigen::Vector3d vec{1.0, 2.0, 3.0};
 		Eigen::MatrixXd jacob = statespace::numericalJacobian(func, vec, 1);
@@ -51,7 +51,7 @@ TEST_CASE("Test Numerical Jacobian") {
 		REQUIRE(jacob.cols() == 3);
 		REQUIRE((jacob - 2 * vec.transpose()).cwiseAbs().maxCoeff() <= 1e-4);
 	}
-	{
+	SECTION("Test constant function") {
 		auto func = [](const Eigen::Vector2d& x) { return Eigen::Vector4d::Ones(); };
 		Eigen::Vector2d vec = Eigen::Vector2d::Random();
 		Eigen::MatrixXd jacob = statespace::numericalJacobian(func, vec, 4);
@@ -59,14 +59,16 @@ TEST_CASE("Test Numerical Jacobian") {
 		REQUIRE(jacob.cols() == 2);
 		REQUIRE((jacob - Eigen::Matrix<double, 4, 2>::Zero()).cwiseAbs().maxCoeff() <= 1e-4);
 	}
-	for (int i = 0; i < 20; i++) {
-		constexpr int output = 5, input = 10;
-		Eigen::Matrix<double, output, input> mat = Eigen::Matrix<double, output, input>::Random();
-		auto func = [&mat](const Eigen::VectorXd& x) { return mat * x; };
-		Eigen::Matrix<double, input, 1> vec = Eigen::Matrix<double, input, 1>::Random();
-		Eigen::MatrixXd jacob = statespace::numericalJacobian(func, vec, output);
-		REQUIRE(jacob.rows() == output);
-		REQUIRE(jacob.cols() == input);
-		REQUIRE((jacob - mat).cwiseAbs().maxCoeff() <= 1e-4);
+	SECTION("Test random matrix product") {
+		for (int i = 0; i < 20; i++) {
+			constexpr int output = 5, input = 10;
+			Eigen::Matrix<double, output, input> mat = Eigen::Matrix<double, output, input>::Random();
+			auto func = [&mat](const Eigen::VectorXd& x) { return mat * x; };
+			Eigen::Matrix<double, input, 1> vec = Eigen::Matrix<double, input, 1>::Random();
+			Eigen::MatrixXd jacob = statespace::numericalJacobian(func, vec, output);
+			REQUIRE(jacob.rows() == output);
+			REQUIRE(jacob.cols() == input);
+			REQUIRE((jacob - mat).cwiseAbs().maxCoeff() <= 1e-4);
+		}
 	}
 }
