fc0e0e91b28eb3d70c73ad0dbaa4ad47289c54b5
Evan Straw <evan.straw99@gmail.com>
2022-05-27 17:25:47 -0700

    Get rid of `WITH_CV` option in CMakeLists.txt
    It's not really going to be practical for us to stub out CV-related
    code anymore, since we require OpenCV in all cases anyway due to the
    world interface (and trying to find a solution to allow that to be
    stubbed out would be harder than just requiring OpenCV on all
    systems). Ubuntu has a sufficient version of OpenCV packaged in its
    catalog anyway so we won't have the problem that inspired the stubbing
    in the first place (we were trying to run the rover for a demo and
    couldn't get it to compile because OpenCV was out of date).

--- src/network/MissionControlProtocol.cpp
+++ src/network/MissionControlProtocol.cpp
@@ -10,6 +10,8 @@
 #include <unordered_map>
 #include <unordered_set>
 
+#include "../base64/base64_img.h"
+
 namespace net {
 namespace mc {
 
@@ -204,6 +206,35 @@ MissionControlProtocol::~MissionControlProtocol() {
 	this->_streaming_running = false;
 	if (this->_streaming_thread.joinable()) {
 		this->_streaming_thread.join();
+	}
+}
+
+
+void MissionControlProtocol::videoStreamTask() {
+	while (this->_streaming_running) {
+		std::shared_lock<std::shared_mutex> stream_lock(this->_stream_mutex);
+		// for all open streams, check if there is a new frame
+		for (const auto& stream : _open_streams) {
+			const CameraID& cam = stream.first;
+			const uint32_t& frame_num = stream.second;
+			if (robot::hasNewCameraFrame(cam, frame_num)) {
+				// if there is a new frame, grab it
+				auto data = robot::readCamera(cam).getData();
+				uint32_t& new_frame_num = data.second;
+				cv::Mat frame = data.first;
+				// update the previous frame number
+				this->_open_streams[cam] = new_frame_num;
+
+				// convert frame to base64 and send it
+				std::string b64_data = base64::encodeMat(frame, ".jpg");
+				sendCameraStreamReport(cam, b64_data);
+			}
+
+			// break out of the loop if we should stop streaming
+			if (!this->_streaming_running) {
+				break;
+			}
+		}
 	}
 }
 
