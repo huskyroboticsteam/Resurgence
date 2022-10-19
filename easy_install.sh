0f73d6487ae26210ad73a23ab88eb8cdbef7110c
Abhay D <a.deshpande012@gmail.com>
2022-10-18 21:00:52 -0700

    Fix minor bug in easy install

--- easy_install.sh
+++ easy_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo apt install curl wget
+sudo apt install -y curl wget
 sudo curl -s --compressed "https://huskyroboticsteam.github.io/ubuntu-repo/KEY.gpg" | sudo apt-key add -
 sudo wget -P /etc/apt/sources.list.d "https://huskyroboticsteam.github.io/ubuntu-repo/husky_robotics.list"
+sudo apt update
 sudo apt install -y build-essential unzip gnupg2 lsb-release git \
 	cmake libeigen3-dev libopencv-dev libopencv-contrib-dev \
 	libwebsocketpp-dev libboost-system-dev gpsd gpsd-clients libgps-dev nlohmann-json3-dev \
