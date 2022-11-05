fafe925d97c1379659fc6bc6725fd45ca97982b5
simranmalhi <simranmalhi@users.noreply.github.com>
2022-11-05 20:32:53 +0000

    deploy: dfcd44d0cf38057f170c62d9122fcff1b837bf31

--- search/classes_a.js
+++ search/classes_a.js
@@ -1,10 +1,12 @@
 var searchData=
 [
-  ['jacobirotation_0',['JacobiRotation',['https://eigen.tuxfamily.org/dox/classEigen_1_1JacobiRotation.html',1,'Eigen']]],
-  ['jacobisvd_1',['JacobiSVD',['https://eigen.tuxfamily.org/dox/classEigen_1_1JacobiSVD.html',1,'Eigen']]],
-  ['join_2',['join',['https://docs.opencv.org/4.2.0/d0/de2/structcv_1_1datasets_1_1join.html',1,'cv::datasets']]],
-  ['join_5ferror_3',['join_error',['https://gcc.gnu.org/onlinedocs/gcc-9.3.0/libstdc++/api/a11135.html',1,'__gnu_pbds']]],
-  ['jointpos_5ft_4',['jointpos_t',['../structjointpos__t.html',1,'']]],
-  ['json_5fpointer_5',['json_pointer',['https://nlohmann.github.io/json/doxygen/classnlohmann_1_1json__pointer.html',1,'nlohmann']]],
-  ['json_5fsax_6',['json_sax',['https://nlohmann.github.io/json/doxygen/structnlohmann_1_1json__sax.html',1,'nlohmann']]]
+  ['jacobianposcontroller_0',['JacobianPosController',['../classcontrol_1_1JacobianPosController.html',1,'control']]],
+  ['jacobianvelcontroller_1',['JacobianVelController',['../classJacobianVelController.html',1,'']]],
+  ['jacobirotation_2',['JacobiRotation',['https://eigen.tuxfamily.org/dox/classEigen_1_1JacobiRotation.html',1,'Eigen']]],
+  ['jacobisvd_3',['JacobiSVD',['https://eigen.tuxfamily.org/dox/classEigen_1_1JacobiSVD.html',1,'Eigen']]],
+  ['join_4',['join',['https://docs.opencv.org/4.2.0/d0/de2/structcv_1_1datasets_1_1join.html',1,'cv::datasets']]],
+  ['join_5ferror_5',['join_error',['https://gcc.gnu.org/onlinedocs/gcc-9.3.0/libstdc++/api/a11135.html',1,'__gnu_pbds']]],
+  ['jointpos_5ft_6',['jointpos_t',['../structjointpos__t.html',1,'']]],
+  ['json_5fpointer_7',['json_pointer',['https://nlohmann.github.io/json/doxygen/classnlohmann_1_1json__pointer.html',1,'nlohmann']]],
+  ['json_5fsax_8',['json_sax',['https://nlohmann.github.io/json/doxygen/structnlohmann_1_1json__sax.html',1,'nlohmann']]]
 ];
