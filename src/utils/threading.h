3e49e4c23776563e41bbe6b71da0c922117bae5e
Abhay Deshpande <a.deshpande012@gmail.com>
2023-01-19 18:33:23 -0800

    Added PeriodicScheduler class (#193)
    * Added PeriodicScheduler and test
    
    * Changed PeriodicScheduler to use custom clock, added latch impl
    
    * Renamed latch file to threading
    
    * Added removeEvent method
    
    * Improve clear
    
    * Delete copy op and ctor, address pr reviews

---
+++ src/utils/threading.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+
+namespace util {
+
+/**
+ * @brief Implementation of a countdown latch for threading synchronization.
+ *
+ * This has roughly the same interface as std::latch, but that requires C++20,
+ * which we do not support.
+ *
+ * @see https://en.cppreference.com/w/cpp/thread/latch
+ */
+class latch {
+public:
+	/**
+	 * @brief Create a new latch.
+	 *
+	 * @param count This is the number of countdowns before the latch is unlocked.
+	 */
+	latch(std::size_t count);
+
+	latch(const latch&) = delete;
+
+	latch& operator=(const latch&) = delete;
+
+	/**
+	 * @brief Wait until the latch is unlocked.
+	 */
+	void wait() const;
+
+	/**
+	 * @brief Wait until the latch is unlocked, with a timeout.
+	 *
+	 * @param dur The timeout duration.
+	 * @return bool True if the latch was unlocked, false if timed out.
+	 */
+	template <typename Rep, typename Period>
+	bool wait_for(const std::chrono::duration<Rep, Period>& dur) const {
+		std::unique_lock lock(mutex);
+		return cv.wait_for(lock, dur, [&]() { return count <= 0; });
+	}
+
+	/**
+	 * @brief Wait until the latch is unlocked until a specific timepoint.
+	 *
+	 * @param tp The timepoint to wait until.
+	 * @return bool True iff the latch was unlocked before the given time.
+	 */
+	template <typename Clock, typename Duration>
+	bool wait_until(const std::chrono::time_point<Clock, Duration>& tp) const {
+		std::unique_lock lock(mutex);
+		return cv.wait_until(lock, tp, [&]() { return count <= 0; });
+	}
+
+	/**
+	 * @brief Counts down the internal counter.
+	 *
+	 * When the counter reaches 0 the latch is unlocked.
+	 *
+	 * @param n The amount to decrement by.
+	 */
+	void count_down(std::size_t n = 1);
+
+private:
+	std::size_t count;
+	mutable std::mutex mutex;
+	mutable std::condition_variable cv;
+};
+
+} // namespace util
---
+++ tests/util/SchedulerTest.cpp
@@ -0,0 +1,89 @@
+#include "../../src/utils/scheduler.h"
+
+#include "../../src/utils/threading.h"
+
+#include <chrono>
+#include <memory>
+
+#include <catch2/catch.hpp>
+
+using namespace util;
+using namespace std::chrono_literals;
+
+namespace {
+
+class fake_clock {
+public:
+	using duration = std::chrono::milliseconds;
+	using rep = duration::rep;
+	using period = duration::period;
+	using time_point = std::chrono::time_point<fake_clock>;
+	static inline constexpr bool is_steady = false;
+
+	static void advance(duration d) noexcept {
+		now_ += d;
+	}
+	static void reset_to_epoch() noexcept {
+		now_ = decltype(now_)();
+	}
+	static time_point now() noexcept {
+		return now_;
+	}
+
+private:
+	fake_clock() = delete;
+	~fake_clock() = delete;
+	fake_clock(fake_clock const&) = delete;
+
+	static inline time_point now_;
+};
+
+void advanceFakeClock(std::chrono::milliseconds dur, std::chrono::milliseconds inc,
+					  PeriodicScheduler<fake_clock>& scheduler) {
+	for (int i = 0; i < dur / inc; i++) {
+		fake_clock::advance(inc);
+		util::impl::notifyScheduler(scheduler);
+	}
+}
+} // namespace
+
+TEST_CASE("Test PeriodicScheduler", "[util][scheduler]") {
+	PeriodicScheduler<fake_clock> sched;
+	auto i = std::make_shared<latch>(2);
+	auto j = std::make_shared<latch>(3);
+	sched.scheduleEvent(100ms, [&]() { i->count_down(); });
+	sched.scheduleEvent(75ms, [&]() { j->count_down(); });
+	advanceFakeClock(250ms, 5ms, sched);
+	// check that i was counted down twice and j thrice
+	REQUIRE(i->wait_for(100ms));
+	REQUIRE(j->wait_for(100ms));
+
+	sched.clear();
+
+	i = std::make_shared<latch>(1);
+	j = std::make_shared<latch>(1);
+	advanceFakeClock(250ms, 5ms, sched);
+	// check that neither i nor j were counted down
+	REQUIRE_FALSE(i->wait_for(100ms));
+	REQUIRE_FALSE(j->wait_for(100ms));
+
+	sched.scheduleEvent(100ms, [&]() { i->count_down(); });
+	advanceFakeClock(150ms, 5ms, sched);
+	// check that i was counted down but not j
+	REQUIRE(i->wait_for(100ms));
+	REQUIRE_FALSE(j->wait_for(100ms));
+}
+
+TEST_CASE("Test PeriodicScheduler Remove", "[util][scheduler]") {
+	PeriodicScheduler<fake_clock> sched;
+	auto i = std::make_shared<latch>(1);
+	auto id = sched.scheduleEvent(100ms, [&]() { i->count_down(); });
+	advanceFakeClock(100ms, 5ms, sched);
+
+	REQUIRE(i->wait_for(100ms));
+
+	sched.removeEvent(id);
+	i = std::make_shared<latch>(1);
+	advanceFakeClock(100ms, 5ms, sched);
+	REQUIRE_FALSE(i->wait_for(100ms));
+}
