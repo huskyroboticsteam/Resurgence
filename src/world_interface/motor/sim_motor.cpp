610309517c93db2fa1f5f1c1e4aae1968b91c4cf
simranmalhi <simran_malhi@outlook.com>
2023-02-25 13:02:38 -0800

    Fixed sim motor style

--- src/world_interface/motor/sim_motor.cpp
+++ src/world_interface/motor/sim_motor.cpp
@@ -6,34 +6,33 @@
 using nlohmann::json;
 
 namespace robot {
-	sim_motor::sim_motor(robot::types::motorid_t motor, bool hasPosSensor, const std::string& name,
-			  const std::string& path)
-		: base_motor(motor, hasPosSensor), motor_name(name), protocol_path(path) {}
+sim_motor::sim_motor(robot::types::motorid_t motor, bool hasPosSensor, const std::string& name,
+					 const std::string& path)
+	: base_motor(motor, hasPosSensor), motor_name(name), protocol_path(path) {}
 
-	void sim_motor::setMotorPower(double power) {
-		// unschedule velocity event if exists
-		unscheduleVelocityEvent();
+void sim_motor::setMotorPower(double power) {
+	// unschedule velocity event if exists
+	unscheduleVelocityEvent();
 
-		json msg = {{"type", "simMotorPowerRequest"}, {"motor", motor_name}, {"power", power}};
-		sendJSON(msg);
-	}
+	json msg = {{"type", "simMotorPowerRequest"}, {"motor", motor_name}, {"power", power}};
+	sendJSON(msg);
+}
 
-	void sim_motor::setMotorPos(int32_t targetPos) {
-		// unschedule velocity event if exists
-		unscheduleVelocityEvent();
+void sim_motor::setMotorPos(int32_t targetPos) {
+	// unschedule velocity event if exists
+	unscheduleVelocityEvent();
 
-		json msg = {{"type", "simMotorPositionRequest"},
-					{"motor", motor_name},
-					{"position", targetPos}};
-		sendJSON(msg);
-	}
+	json msg = {
+		{"type", "simMotorPositionRequest"}, {"motor", motor_name}, {"position", targetPos}};
+	sendJSON(msg);
+}
 
-	types::DataPoint<int32_t> sim_motor::getMotorPos() const {
-		// this calles the method implementation in world interface
-		return robot::getMotorPos(motor_id);
-	}
+types::DataPoint<int32_t> sim_motor::getMotorPos() const {
+	// this calles the method implementation in world interface
+	return robot::getMotorPos(motor_id);
+}
 
-	void sim_motor::sendJSON(const json& obj) {
-		Globals::websocketServer.sendJSON(protocol_path, obj);
-	}
+void sim_motor::sendJSON(const json& obj) {
+	Globals::websocketServer.sendJSON(protocol_path, obj);
+}
 } // namespace robot
