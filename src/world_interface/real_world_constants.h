b63cd626e451d2b616f5fd9ab9f13278ecdedae9
Abhay Deshpande <a.deshpande012@gmail.com>
2023-02-25 13:16:00 -0800

    Tune arm pwm (#221)

--- src/world_interface/real_world_constants.h
+++ src/world_interface/real_world_constants.h
@@ -93,10 +93,10 @@ constexpr auto motorPIDMap =
  * Negative values mean that the motor is inverted.
  */
 constexpr auto positive_pwm_scales =
-	frozen::make_unordered_map<motorid_t, double>({{motorid_t::armBase, -0.5},
+	frozen::make_unordered_map<motorid_t, double>({{motorid_t::armBase, -0.75},
 												   {motorid_t::shoulder, 1},
-												   {motorid_t::elbow, -0.7},
-												   {motorid_t::forearm, 0.5},
+												   {motorid_t::elbow, -1},
+												   {motorid_t::forearm, 0.65},
 												   {motorid_t::wrist, 0.6},
 												   {motorid_t::frontLeftWheel, 0.7},
 												   {motorid_t::frontRightWheel, -0.7},
@@ -108,10 +108,10 @@ constexpr auto positive_pwm_scales =
  * Negative values mean that the motor is inverted.
  */
 constexpr auto negative_pwm_scales =
-	frozen::make_unordered_map<motorid_t, double>({{motorid_t::armBase, -0.5},
+	frozen::make_unordered_map<motorid_t, double>({{motorid_t::armBase, -0.75},
 												   {motorid_t::shoulder, 1},
 												   {motorid_t::elbow, -1},
-												   {motorid_t::forearm, 0.5},
+												   {motorid_t::forearm, 0.65},
 												   {motorid_t::wrist, 0.6},
 												   {motorid_t::frontLeftWheel, 0.7},
 												   {motorid_t::frontRightWheel, -0.7},
