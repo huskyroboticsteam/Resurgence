1a6987b60579ea8445f34f13f470db4a5dd0b16f
huttongrabiel <huttonthomas@icloud.com>
2023-04-24 17:16:08 -0700

    add sudo to required copies

--- rover-config/rover-config.sh
+++ rover-config/rover-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cp -r ./.motion/ /home/$USER
-cp ./50-rover-cameras.rules /etc/udev/rules.d/
-cp ./50-usb-hokuyo-lidar.rules /etc/udev/rules.d/
+sudo cp ./50-rover-cameras.rules /etc/udev/rules.d/
+sudo cp ./50-usb-hokuyo-lidar.rules /etc/udev/rules.d/
 
 # Reloads udev rules instead of having to reboot
 sudo udevadm control --reload-rules
