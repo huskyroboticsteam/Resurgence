f96fa21d6f1018528906ac4842fc64211322f379
abhaybd <abhaybd@users.noreply.github.com>
2023-05-05 02:28:07 +0000

    deploy: 3d32a78f7e72ef3f8f957cd3cdd5a05f897faf81

--- search/classes_18.js
+++ search/classes_18.js
@@ -1,12 +1,13 @@
 var searchData=
 [
-  ['xyz2rgb_0',['XYZ2RGB',['https://docs.opencv.org/4.2.0/df/d94/classcv_1_1cudev_1_1color__cvt__detail_1_1XYZ2RGB.html',1,'cv::cudev::color_cvt_detail']]],
-  ['xyz4_5fto_5fbgr_5ffunc_1',['XYZ4_to_BGR_func',['https://docs.opencv.org/4.2.0/df/d79/structcv_1_1cudev_1_1XYZ4__to__BGR__func.html',1,'cv::cudev']]],
-  ['xyz4_5fto_5fbgra_5ffunc_2',['XYZ4_to_BGRA_func',['https://docs.opencv.org/4.2.0/d9/db8/structcv_1_1cudev_1_1XYZ4__to__BGRA__func.html',1,'cv::cudev']]],
-  ['xyz4_5fto_5frgb_5ffunc_3',['XYZ4_to_RGB_func',['https://docs.opencv.org/4.2.0/d5/d93/structcv_1_1cudev_1_1XYZ4__to__RGB__func.html',1,'cv::cudev']]],
-  ['xyz4_5fto_5frgba_5ffunc_4',['XYZ4_to_RGBA_func',['https://docs.opencv.org/4.2.0/d4/d85/structcv_1_1cudev_1_1XYZ4__to__RGBA__func.html',1,'cv::cudev']]],
-  ['xyz_5fto_5fbgr_5ffunc_5',['XYZ_to_BGR_func',['https://docs.opencv.org/4.2.0/d1/d18/structcv_1_1cudev_1_1XYZ__to__BGR__func.html',1,'cv::cudev']]],
-  ['xyz_5fto_5fbgra_5ffunc_6',['XYZ_to_BGRA_func',['https://docs.opencv.org/4.2.0/dd/d9e/structcv_1_1cudev_1_1XYZ__to__BGRA__func.html',1,'cv::cudev']]],
-  ['xyz_5fto_5frgb_5ffunc_7',['XYZ_to_RGB_func',['https://docs.opencv.org/4.2.0/de/da3/structcv_1_1cudev_1_1XYZ__to__RGB__func.html',1,'cv::cudev']]],
-  ['xyz_5fto_5frgba_5ffunc_8',['XYZ_to_RGBA_func',['https://docs.opencv.org/4.2.0/d8/dfd/structcv_1_1cudev_1_1XYZ__to__RGBA__func.html',1,'cv::cudev']]]
+  ['x264_5fnal_5ft_5fsimple_0',['x264_nal_t_simple',['../structh264encoder_1_1x264__nal__t__simple.html',1,'h264encoder']]],
+  ['xyz2rgb_1',['XYZ2RGB',['https://docs.opencv.org/4.2.0/df/d94/classcv_1_1cudev_1_1color__cvt__detail_1_1XYZ2RGB.html',1,'cv::cudev::color_cvt_detail']]],
+  ['xyz4_5fto_5fbgr_5ffunc_2',['XYZ4_to_BGR_func',['https://docs.opencv.org/4.2.0/df/d79/structcv_1_1cudev_1_1XYZ4__to__BGR__func.html',1,'cv::cudev']]],
+  ['xyz4_5fto_5fbgra_5ffunc_3',['XYZ4_to_BGRA_func',['https://docs.opencv.org/4.2.0/d9/db8/structcv_1_1cudev_1_1XYZ4__to__BGRA__func.html',1,'cv::cudev']]],
+  ['xyz4_5fto_5frgb_5ffunc_4',['XYZ4_to_RGB_func',['https://docs.opencv.org/4.2.0/d5/d93/structcv_1_1cudev_1_1XYZ4__to__RGB__func.html',1,'cv::cudev']]],
+  ['xyz4_5fto_5frgba_5ffunc_5',['XYZ4_to_RGBA_func',['https://docs.opencv.org/4.2.0/d4/d85/structcv_1_1cudev_1_1XYZ4__to__RGBA__func.html',1,'cv::cudev']]],
+  ['xyz_5fto_5fbgr_5ffunc_6',['XYZ_to_BGR_func',['https://docs.opencv.org/4.2.0/d1/d18/structcv_1_1cudev_1_1XYZ__to__BGR__func.html',1,'cv::cudev']]],
+  ['xyz_5fto_5fbgra_5ffunc_7',['XYZ_to_BGRA_func',['https://docs.opencv.org/4.2.0/dd/d9e/structcv_1_1cudev_1_1XYZ__to__BGRA__func.html',1,'cv::cudev']]],
+  ['xyz_5fto_5frgb_5ffunc_8',['XYZ_to_RGB_func',['https://docs.opencv.org/4.2.0/de/da3/structcv_1_1cudev_1_1XYZ__to__RGB__func.html',1,'cv::cudev']]],
+  ['xyz_5fto_5frgba_5ffunc_9',['XYZ_to_RGBA_func',['https://docs.opencv.org/4.2.0/d8/dfd/structcv_1_1cudev_1_1XYZ__to__RGBA__func.html',1,'cv::cudev']]]
 ];
