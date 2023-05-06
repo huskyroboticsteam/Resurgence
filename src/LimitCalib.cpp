a52bff41ebe45f23693641225306c56d0123e268
AyushKulk <ayush.kulkarni04@gmail.com>
2023-05-06 12:14:28 -0700

    format fix v5

--- src/LimitCalib.cpp
+++ src/LimitCalib.cpp
@@ -1,6 +1,6 @@
 #include "world_interface/world_interface.h"
 
 int main() {
-        bool runCalibration = true;
-        robot::world_interface_init(runCalibration);
+       bool runCalibration = true;
+       robot::world_interface_init(runCalibration);
 }
