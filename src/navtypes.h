68c25c5711c9039a7d223a39bd2cc05037caade8
huttongrabiel <huttonthomas@icloud.com>
2023-05-11 18:43:36 -0700

    make clang-format happy

--- src/navtypes.h
+++ src/navtypes.h
@@ -23,14 +23,11 @@ using pose_t = Eigen::Vector3d;		 // Robot pose: x, y, theta
 using transform_t = Eigen::Matrix3d; // a pose in matrix form; see toPose below
 using trajectory_t = std::vector<transform_t>;
 
-template <int rows, int cols>
-using Matrixd = Eigen::Matrix<double, rows, cols>;
+template <int rows, int cols> using Matrixd = Eigen::Matrix<double, rows, cols>;
 
-template <int dim>
-using Vectord = Eigen::Matrix<double, dim, 1>;
+template <int dim> using Vectord = Eigen::Matrix<double, dim, 1>;
 
-template <int dim, int dim2=1>
-using Arrayd = Eigen::Array<double, dim, dim2>;
+template <int dim, int dim2 = 1> using Arrayd = Eigen::Array<double, dim, dim2>;
 
 using point_t =
 	Eigen::Vector3d; // x, y, 1 (or 0, 0, 0 to represent "no data")
