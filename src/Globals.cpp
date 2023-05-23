bb634f96c0de88f10b1d37ead7c93dc7fbffadea
Geeoon Chung <geeoon13@gmail.com>
2023-05-22 19:04:36 -0700

    Created constants

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -1,14 +1,19 @@
 #include "Globals.h"
+
 #include "Constants.h"
 #include "world_interface/data.h"
 
-#include <vector>
 #include <atomic>
+#include <vector>
 
 namespace Globals {
 RoverState curr_state;
-net::websocket::SingleClientWSServer websocketServer("DefaultServer", Constants::WS_SERVER_PORT);
+net::websocket::SingleClientWSServer websocketServer("DefaultServer",
+													 Constants::WS_SERVER_PORT);
 std::atomic<bool> E_STOP = false;
 std::atomic<bool> AUTONOMOUS = false;
 robot::types::mountedperipheral_t mountedPeripheral = robot::types::mountedperipheral_t::none;
+kinematics::PlanarArmKinematics<2> planarArmKinematics =
+	kinematics::PlanarArmKinematics<2>({0, 0}, {0, 0}, {0, 0}, 0.0, 0);
+control::PlanarArmController<2> planarArmController = control::PlanarArmController<2>{{0, 0}, planarArmKinematics};
 } // namespace Globals
