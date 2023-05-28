7196503092ede72c866c3177a0ded38ad547ebab
huttongrabiel <huttonthomas@icloud.com>
2023-05-27 17:49:15 -0700

    ArduPilotInterface: Add init function + validate IMU heading

--- src/ardupilot/ArduPilotInterface.cpp
+++ src/ardupilot/ArduPilotInterface.cpp
@@ -1,21 +1,35 @@
-#include "ArduPilotProtocol.h"
+#include "ArduPilotInterface.h"
+
+#include <memory>
 
 using namespace robot::types;
 using namespace navtypes;
 using namespace std::chrono_literals;
 
 namespace {
-net::ardupilot::ArduPilotProtocol ardupilot_protocol;
+
+std::unique_ptr<net::ardupilot::ArduPilotProtocol> ardupilot_protocol;
+
 } // namespace
+
+namespace ardupilot {
+void initArduPilotProtocol(net::websocket::SingleClientWSServer& websocketServer) {
+	std::make_unique<net::ardupilot::ArduPilotProtocol>(websocketServer);
+}
+} // namespace ardupilot
 
 namespace gps {
 DataPoint<gpscoords_t> readGPSCoords() {
-	return ardupilot_protocol.getGPS();
+	return ardupilot_protocol->getGPS();
 }
 } // namespace gps
 
 namespace robot {
 DataPoint<double> readIMUHeading() {
-	return ardupilot_protocol.getIMU().getData().yaw;
+	auto imu_heading = ardupilot_protocol->getIMU();
+	if (imu_heading.isValid()) {
+		return imu_heading.getData().yaw;
+	}
+	return DataPoint<double>();
 }
 } // namespace robot
---
+++ src/ardupilot/ArduPilotInterface.h
@@ -0,0 +1,9 @@
+#include "ArduPilotProtocol.h"
+
+namespace ardupilot {
+/* @brief Creates a new ArduPilotProtocol
+ *
+ * @param A websocket server on which to add the protocol
+ */
+void initArduPilotProtocol(net::websocket::SingleClientWSServer& websocketServer);
+} // namespace ardupilot
