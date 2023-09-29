d7b14ce2290748d86ff653d4ee887beb9f7f65d7
DrDab <duvictor514@gmail.com>
2023-09-28 18:43:06 -0700

    Set setpoint in safety factor test

--- tests/control/PlanarArmControllerTest.cpp
+++ tests/control/PlanarArmControllerTest.cpp
@@ -14,6 +14,11 @@ TEST_CASE("Test Planar Arm Controller", "[control][planararmcontroller]") {
 }
 
 TEST_CASE("Test Planar Arm Safety Factor", "[control][planararmcontroller]") {
+	navtypes::Vectord<2> segLens({2.0, 3.0});
+	navtypes::Vectord<2> minAngles({-M_PI, -M_PI});
+	navtypes::Vectord<2> maxAngles({M_PI, M_PI});
+	kinematics::PlanarArmKinematics<2> kin_obj(segLens, minAngles, maxAngles, 0.0, 0);
+
 	PlanarArmController<2> foo({0, 0}, kin_obj, Constants::arm::SAFETY_FACTOR);
-	
+	foo.set_setpoint({5.0, 0.0});
 }
