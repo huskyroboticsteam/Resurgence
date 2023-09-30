10044f6adadda1d5c3c1f82b1ac0a93a57513ba6
DrDab <duvictor514@gmail.com>
2023-09-30 15:59:03 -0700

    More formatting fixes

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -41,6 +41,7 @@ robot::types::mountedperipheral_t mountedPeripheral = robot::types::mountedperip
 const kinematics::PlanarArmKinematics<Constants::arm::IK_MOTORS.size()>
 	planarArmKinematics(getSegLens(), getJointLimits(true), getJointLimits(false),
 						IK_SOLVER_THRESH, IK_SOLVER_MAX_ITER);
-control::PlanarArmController<2> planarArmController({0, 0}, planarArmKinematics, Constants::arm::SAFETY_FACTOR);
+control::PlanarArmController<2> planarArmController({0, 0}, planarArmKinematics,
+													Constants::arm::SAFETY_FACTOR);
 std::atomic<bool> armIKEnabled = false;
 } // namespace Globals
