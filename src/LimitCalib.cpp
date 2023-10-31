962c234704423172b36ba2c5f0a197755f7a5201
AyushKulk <ayush.kulkarni04@gmail.com>
2023-10-30 18:25:34 -0700

    vfinal

--- src/LimitCalib.cpp
+++ src/LimitCalib.cpp
@@ -1,3 +1,4 @@
+#include "control/TrapezoidalVelocityProfile.h"
 #include "log.h"
 #include "utils/threading.h"
 #include "world_interface/real_world_constants.h"
@@ -5,7 +6,9 @@
 
 #include <chrono>
 #include <csignal>
+#include <iostream>
 
+using namespace Eigen;
 using namespace robot;
 using namespace std::chrono;
 using robot::types::DataPoint;
@@ -53,4 +56,37 @@ int main() {
 	} while (!latch.wait_for(500ms));
 
 	log(LOG_INFO, "Done\n");
+
+	std::array<robot::types::motorid_t, 2> sortedKeys = 
+		{robot::types::motorid_t::shoulder, robot::types::motorid_t::elbow};
+
+	std::sort(sortedKeys.begin(), sortedKeys.end());
+
+	control::TrapezoidalVelocityProfile<encMotors.size()> profile(0.4, 0.6);
+	// std::vector<double, 2> targetPosVec;
+	navtypes::Vectord<2> targetPosVec;
+	std::array<robot::types::motorid_t, 2> motorNames;
+
+	int i = 0;
+	for (const auto& runningMotor : encMotors) {
+		motorNames[i] = runningMotor.first;
+		targetPosVec[i] = runningMotor.second.stdPos;
+		i++;
+	}
+
+	// std::sort(targetPosVec.begin(), targetPosVec.end(), customVecComparator);
+
+	robot::types::datatime_t currTime(0s);
+	profile.setTarget(currTime, getMotorPositionsRad(motorNames), targetPosVec);
+
+	JacobianVelController<2, 2> controller([](Eigen::VectorXd x) { return x; }, {});
+
+	double d;
+	navtypes::Vectord<2> targetVel = profile.getCommand(currTime);
+	navtypes::Vectord<2> targetPos = controller.getCommand(currTime, targetVel, d);
+	i = 0;
+	for (const auto& element : sortedKeys) {
+		robot::setMotorPos(element, targetPos[i]);
+		i++;
+	}
 }
