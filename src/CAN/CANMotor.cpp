938fbe862b31e78bec2974d95488bacb4b3f3bff
DrDab <duvictor514@gmail.com>
2024-04-15 19:43:53 -0700

    Merge CANMotor_PSOC into CANMotor

--- src/CAN/CANMotor.cpp
+++ src/CAN/CANMotor.cpp
@@ -5,7 +5,11 @@
 
 #include <chrono>
 #include <cmath>
+#include <condition_variable>
+#include <mutex>
+#include <queue>
 #include <thread>
+#include <vector>
 
 extern "C" {
 #include <HindsightCAN/CANCommon.h>
@@ -18,6 +22,68 @@ using robot::types::DataPoint;
 using robot::types::LimitSwitchData;
 
 namespace can::motor {
+
+void initEncoder(deviceserial_t serial, bool invertEncoder, bool zeroEncoder,
+				 int32_t pulsesPerJointRev,
+				 std::optional<std::chrono::milliseconds> telemetryPeriod) {
+	auto motorGroupCode = static_cast<uint8_t>(devicegroup_t::motor);
+	CANPacket p;
+	AssembleEncoderInitializePacket(&p, motorGroupCode, serial, sensor_t::encoder,
+									invertEncoder, zeroEncoder);
+	svoid initEncoder(deviceserial_t serial, bool invertEncoder, bool zeroEncoder,
+				 int32_t pulsesPerJointRev,
+				 std::optional<std::chrono::milliseconds> telemetryPeriod) {
+	auto motorGroupCode = static_cast<uint8_t>(devicegroup_t::motor);
+	CANPacket p;
+	AssembleEncoderInitializePacket(&p, motorGroupCode, serial, sensor_t::encoder,
+									invertEncoder, zeroEncoder);
+	sendCANPacket(p);
+	std::this_thread::sleep_for(1000us);
+	AssembleEncoderPPJRSetPacket(&p, motorGroupCode, serial, pulsesPerJointRev);
+	sendCANPacket(p);
+	std::this_thread::sleep_for(1000us);
+	if (telemetryPeriod) {
+		scheduleTelemetryPull(std::make_pair(devicegroup_t::motor, serial), telemtype_t::angle,
+							  telemetryPeriod.value());
+	}
+}
+
+void initPotentiometer(deviceserial_t serial, int32_t posLo, int32_t posHi, uint16_t adcLo,
+					   uint16_t adcHi,
+					   std::optional<std::chrono::milliseconds> telemetryPeriod) {
+	CANPacket packet;
+	auto group = static_cast<uint8_t>(devicegroup_t::motor);
+	AssemblePotHiSetPacket(&packet, group, serial, adcHi, posHi);
+	sendCANPacket(packet);
+	AssemblePotLoSetPacket(&packet, group, serial, adcLo, posLo);
+	sendCANPacket(packet);
+	if (telemetryPeriod) {
+		scheduleTelemetryPull(std::make_pair(devicegroup_t::motor, serial), telemtype_t::angle,
+							  telemetryPeriod.value());
+	}
+}ssembleEncoderPPJRSetPacket(&p, motorGroupCode, serial, pulsesPerJointRev);
+	sendCANPacket(p);
+	std::this_thread::sleep_for(1000us);
+	if (telemetryPeriod) {
+		scheduleTelemetryPull(std::make_pair(devicegroup_t::motor, serial), telemtype_t::angle,
+							  telemetryPeriod.value());
+	}
+}
+
+void initPotentiometer(deviceserial_t serial, int32_t posLo, int32_t posHi, uint16_t adcLo,
+					   uint16_t adcHi,
+					   std::optional<std::chrono::milliseconds> telemetryPeriod) {
+	CANPacket packet;
+	auto group = static_cast<uint8_t>(devicegroup_t::motor);
+	AssemblePotHiSetPacket(&packet, group, serial, adcHi, posHi);
+	sendCANPacket(packet);
+	AssemblePotLoSetPacket(&packet, group, serial, adcLo, posLo);
+	sendCANPacket(packet);
+	if (telemetryPeriod) {
+		scheduleTelemetryPull(std::make_pair(devicegroup_t::motor, serial), telemtype_t::angle,
+							  telemetryPeriod.value());
+	}
+}
 
 void emergencyStopMotors() {
 	CANPacket p;
