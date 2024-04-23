5cae9470e8faa501f4619ed05cda38be63dca82e
Quinn Pfeifer <72775766+QuinnP2910@users.noreply.github.com>
2024-04-22 17:36:58 -0700

    Make anonymous namespace in Constants.cpp for private values

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -5,6 +5,8 @@
 
 namespace Constants {
 namespace Drive {
+
+namespace {
 constexpr std::array<int32_t, 4> NORMAL_WHEEL_ROTS = {0, 0, 0, 0};
 const swervewheelvel_t TURN_IN_PLACE_VEL =
 	robot::swerveKinematics().robotVelToWheelVel(0, 0, 1);
@@ -14,6 +16,8 @@ const std::array<int32_t, 4> TURN_IN_PLACE_WHEEL_ROTS = {
 	static_cast<int32_t>(TURN_IN_PLACE_VEL.lbRot * 1000),
 	static_cast<int32_t>(TURN_IN_PLACE_VEL.rbRot * 1000)};
 constexpr std::array<int32_t, 4> CRAB_WHEEL_ROTS = {90000, 90000, 90000, 90000};
+} // namespace
+
 const extern std::unordered_map<DriveMode, std::array<int32_t, 4>> WHEEL_ROTS = {
 	{DriveMode::Normal, NORMAL_WHEEL_ROTS},
 	{DriveMode::TurnInPlace, TURN_IN_PLACE_WHEEL_ROTS},
