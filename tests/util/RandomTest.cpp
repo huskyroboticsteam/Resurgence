a8de6bc46f11dc47a2494593c17b0e3aca01f2e2
ricemaking <dylandomingo57@gmail.com>
2024-11-04 18:06:25 -0800

    Fixed formatting

--- tests/util/RandomTest.cpp
+++ tests/util/RandomTest.cpp
@@ -5,17 +5,17 @@
 using namespace util;
 
 TEST_CASE("Test Standard Normal Distribution w/ different thread_ids", "[util][random]") {
-    double sample0 = stdn(0);
-    double sample1 = stdn(1);
+	double sample0 = stdn(0);
+	double sample1 = stdn(1);
 
-    // samples should be different
-    REQUIRE(sample0 != sample1);
+	// samples should be different
+	REQUIRE(sample0 != sample1);
 }
 
 TEST_CASE("Test Get Normal Seed", "[util][random]") {
-    double seed1 = getNormalSeed();
-    double seed2 = getNormalSeed();
+	double seed1 = getNormalSeed();
+	double seed2 = getNormalSeed();
 
-    // seeds should be the same
-    REQUIRE(seed1 == seed2);
+	// seeds should be the same
+	REQUIRE(seed1 == seed2);
 }
