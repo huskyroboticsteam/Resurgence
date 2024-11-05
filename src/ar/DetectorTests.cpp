03581e5e06bc6d5fb5adba0ca2998852f159fa7a
Isaac <isaacw1925@gmail.com>
2024-11-04 17:42:13 -0800

    Remove all ar/

--- src/ar/DetectorTests.cpp
+++
@@ -1,9 +0,0 @@
-#include "Detector.h"
-
-#include <catch2/catch.hpp>
-
-namespace AR {
-// TODO add Detector tests later. This is sort of a "nice to have" feature; we can already
-// test the detector manually using the ar_tester program, but this would allow us to test
-// certain limited cases automatically as part of our test suite.
-} // namespace AR
