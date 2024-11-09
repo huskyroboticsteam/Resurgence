9beb952fdd1e58fdd26f4443617b5e679dec6849
Geeoon Chung <geeoon13@gmail.com>
2024-11-09 11:19:35 -0800

    Fixed formatting error

--- src/TunePID.cpp
+++ src/TunePID.cpp
@@ -166,7 +166,8 @@ int main(int argc, char** argv) {
 		if (mode == targetmode_t::step) {
 			prescaled_target = round(prescaled_target);
 		}
-		angle_target = static_cast<int32_t>(round(amplitude * prescaled_target)) + starting_angle;
+		angle_target =
+			static_cast<int32_t>(round(amplitude * prescaled_target)) + starting_angle;
 
 		can::motor::setMotorPIDTarget(serial, angle_target);
 
