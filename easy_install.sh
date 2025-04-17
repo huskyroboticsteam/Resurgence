8215ebb9f62f8e59e1d5beddcf89a466488192a1
Isaac <isaacw1925@gmail.com>
2025-04-17 15:28:15 -0700

    Update easy_install.sh

--- easy_install.sh
+++ easy_install.sh
@@ -4,9 +4,9 @@ sudo apt install -y curl wget
 
 # Transfer key
 sudo mkdir -p /etc/apt/keyrings/
-sudo wget /etc/apt/keyrings/ "https://huskyroboticsteam.github.io/ubuntu-repo/KEY.gpg"
+sudo wget -P /etc/apt/keyrings/ "https://huskyroboticsteam.github.io/ubuntu-repo/KEY.gpg"
 sudo gpg --no-default-keyring --keyring ./temp-keyring.gpg --import /etc/apt/keyrings/KEY.gpg
-sudo gpg --no-default-keyring --keyring ./temp-keyring.gpg --export --output /etc/apt/keyrings/KEY.gpg
+sudo gpg --no-default-keyring --keyring ./temp-keyring.gpg --export --output /etc/apt/keyrings/KEY.gpg --yes
 sudo rm temp-keyring.gpg
 
 sudo wget -P /etc/apt/sources.list.d "https://huskyroboticsteam.github.io/ubuntu-repo/husky_robotics.list"
