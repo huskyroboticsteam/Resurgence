0d9ec323e55e7aa6f1838708dbbbdb4301115fd8
Isaac <isaacw1925@gmail.com>
2025-04-17 15:00:20 -0700

    Update easy_install.sh to stop using possibly unsafe apt-key

--- easy_install.sh
+++ easy_install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 sudo apt install -y curl wget
-sudo curl -s --compressed "https://huskyroboticsteam.github.io/ubuntu-repo/KEY.gpg" | sudo apt-key add -
-sudo wget -P /etc/apt/sources.list.d "https://huskyroboticsteam.github.io/ubuntu-repo/husky_robotics.list"
+
+# Transfer key
+sudo mkdir -p /etc/apt/keyrings/
+sudo wget /etc/apt/keyrings/ "https://huskyroboticsteam.github.io/ubuntu-repo/KEY.gpg"
+sudo gpg --nodefault-keyring --keyring ./temp-keyring.gpg --import KEY.gpg
+sudo gpg --nodefault-keyring --keyring ./temp-keyring.gpg --export --output KEY.gpg
+sudo rm temp-keyring.gpg
+
+# sudo wget -P /etc/apt/sources.list.d "https://huskyroboticsteam.github.io/ubuntu-repo/husky_robotics.list"
+echo "deb [signed-by=/etc/apt/keyrings/KEY.gpg] https://huskyroboticsteam.github.io/ubuntu-repo ./" > /etc/apt/sources.list.d/husky_robotics.list
 ./update_deps.sh
 
 echo "Done installing Resurgence dependencies!"
