b2557f5f1810d5b0b4232d6301b2c345e12f154d
Isaac <isaacw1925@gmail.com>
2025-04-17 15:04:00 -0700

    Fix typo

--- easy_install.sh
+++ easy_install.sh
@@ -5,8 +5,8 @@ sudo apt install -y curl wget
 # Transfer key
 sudo mkdir -p /etc/apt/keyrings/
 sudo wget /etc/apt/keyrings/ "https://huskyroboticsteam.github.io/ubuntu-repo/KEY.gpg"
-sudo gpg --nodefault-keyring --keyring ./temp-keyring.gpg --import KEY.gpg
-sudo gpg --nodefault-keyring --keyring ./temp-keyring.gpg --export --output KEY.gpg
+sudo gpg --no-default-keyring --keyring ./temp-keyring.gpg --import KEY.gpg
+sudo gpg --no-default-keyring --keyring ./temp-keyring.gpg --export --output KEY.gpg
 sudo rm temp-keyring.gpg
 
 # sudo wget -P /etc/apt/sources.list.d "https://huskyroboticsteam.github.io/ubuntu-repo/husky_robotics.list"
