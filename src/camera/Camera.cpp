1af6ba2f2c2cc4ab8128274c847a0af044b8216f
Isaac <isaacw1925@gmail.com>
2025-08-09 16:38:27 +0000

    Fix detection

--- src/camera/Camera.cpp
+++ src/camera/Camera.cpp
@@ -105,31 +105,22 @@ void Camera::captureLoop() {
 		bool success = _capture->read(frame);
 		_capture_lock->unlock();
 		if (success && !frame.empty()) {
-      if (std::strcmp(_name.c_str(), "hand") == 0) {
-        cv::Ptr<cv::aruco::Dictionary> dictionary =
-          cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_100);
-        std::vector<std::vector<cv::Point2f>> markerCorners;
-        cv::Mat frameCopy;
-        std::vector<int> markerIds;
-        cv::aruco::detectMarkers(frame, dictionary, markerCorners, markerIds);
-        frame.copyTo(frameCopy);
-        if(!markerIds.empty()) {
-          cv::aruco::drawDetectedMarkers(frameCopy, markerCorners, markerIds);
-        }
-
-        _frame_lock->lock();
-        frameCopy.copyTo(*(this->_frame));
-        (*_frame_num)++;
-        *_frame_time = dataclock::now();
-        _frame_lock->unlock();
-      } else {
-        _frame_lock->lock();
-        frame.copyTo(*(this->_frame));
-        (*_frame_num)++;
-        *_frame_time = dataclock::now();
-        _frame_lock->unlock();
+      cv::Ptr<cv::aruco::Dictionary> dictionary =
+        cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_100);
+      std::vector<std::vector<cv::Point2f>> markerCorners;
+      cv::Mat frameCopy;
+      std::vector<int> markerIds;
+      cv::aruco::detectMarkers(frame, dictionary, markerCorners, markerIds);
+      frame.copyTo(frameCopy);
+      if(!markerIds.empty()) {
+        cv::aruco::drawDetectedMarkers(frameCopy, markerCorners, markerIds);
       }
 
+      _frame_lock->lock();
+      frameCopy.copyTo(*(this->_frame));
+      (*_frame_num)++;
+      *_frame_time = dataclock::now();
+      _frame_lock->unlock();
 		}
 	}
 }
