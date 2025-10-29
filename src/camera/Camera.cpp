29630578455d70e2381495f4702159775a84bcd2
vabhi <vabhi2007@gmail.com>
2025-10-28 18:02:42 -0700

    Update Camera.cpp

--- src/camera/Camera.cpp
+++ src/camera/Camera.cpp
@@ -90,9 +90,9 @@ std::string Camera::getGSTPipe(CameraID camera_id) {
 	gstr_ss << ",framerate=" << static_cast<int>(fs[KEY_FRAMERATE]) << "/1 ! ";
 
 	if (format == "image/jpeg") {
-		gstr_ss << "jpegdec ! ";
+    gstr_ss << "nvjpegdec ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! ";
 	}
-	gstr_ss << "videoconvert ! appsink";
+	gstr_ss << "appsink";
 
 	return gstr_ss.str();
 }
