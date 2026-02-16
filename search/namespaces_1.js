3ffb6ee1bebc859be764f8fb47ef51bb1096461d
imisaacwu <imisaacwu@users.noreply.github.com>
2026-02-16 20:08:16 +0000

    deploy: f014bea5eed5e40924ba4f238a0415dabb2597c3

--- search/namespaces_1.js
+++ search/namespaces_1.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['abi_0',['abi',['https://gcc.gnu.org/onlinedocs/gcc-9.3.0/libstdc++/api/a01558.html',1,'']]],
-  ['arm_1',['Arm',['../namespaceArm.html',1,'']]]
+  ['abi_0',['abi',['https://gcc.gnu.org/onlinedocs/gcc-9.3.0/libstdc++/api/a01558.html',1,'']]]
 ];
--- search/variables_a.js
+++ search/variables_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['joint_5fmotor_5fmap_0',['JOINT_MOTOR_MAP',['../namespaceConstants.html#ab4129aa720805c5daf9a51d2a8834006',1,'Constants']]],
-  ['jpeg_1',['JPEG',['https://docs.opencv.org/4.2.0/d0/d61/group__cudacodec.html#gga71943a1181287609b5d649f53ce6c146a766712d578e6a32b7def99fb0e17ae4c',1,'cv::cudacodec::JPEG'],['https://docs.opencv.org/4.2.0/d0/d61/group__cudacodec.html#gga71943a1181287609b5d649f53ce6c146a766712d578e6a32b7def99fb0e17ae4c',1,'JPEG(Global Namespace)']]]
+  ['joint_5flimits_0',['JOINT_LIMITS',['../namespaceConstants_1_1Arm.html#aefaa03df5fbe1c7b2475a56e6779da1a',1,'Constants::Arm']]],
+  ['joint_5fmotor_5fmap_1',['JOINT_MOTOR_MAP',['../namespaceConstants.html#ab4129aa720805c5daf9a51d2a8834006',1,'Constants']]],
+  ['jpeg_2',['JPEG',['https://docs.opencv.org/4.2.0/d0/d61/group__cudacodec.html#gga71943a1181287609b5d649f53ce6c146a766712d578e6a32b7def99fb0e17ae4c',1,'cv::cudacodec::JPEG'],['https://docs.opencv.org/4.2.0/d0/d61/group__cudacodec.html#gga71943a1181287609b5d649f53ce6c146a766712d578e6a32b7def99fb0e17ae4c',1,'JPEG(Global Namespace)']]]
 ];
