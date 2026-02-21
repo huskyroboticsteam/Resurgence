70a100bdc9c7d8fc40d0dbf422dce558e380787d
vabhi <vabhi2007@gmail.com>
2026-02-20 18:12:25 -0800

    Update Camera.cpp

--- src/camera/Camera.cpp
+++ src/camera/Camera.cpp
@@ -99,9 +99,7 @@ std::string Camera::getGSTPipe(CameraID camera_id) {
 	gstr_ss << ",height=" << static_cast<int>(fs[KEY_IMAGE_HEIGHT]);
 	gstr_ss << ",framerate=" << static_cast<int>(fs[KEY_FRAMERATE]) << "/1 ! ";
 
-	if (formatLower.find("h264") != std::string::npos) {
-		gstr_ss << "h264parse ! avdec_h264 ! ";
-	} else if (formatLower.find("jpeg") != std::string::npos) {
+	if (formatLower.find("jpeg") != std::string::npos) {
 		gstr_ss << "jpegdec ! ";
 	}
 
