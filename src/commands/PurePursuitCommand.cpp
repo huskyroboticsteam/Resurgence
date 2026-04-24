7a439da8a1db876df8531f640e7052555a88a25c
Ellie Lim <limellie@outlook.com>
2026-04-24 01:46:49 +0000

    Finished pure pursuit implementation in theory. To-do: + connect to AutonomousTask via removing individual point nav

--- src/commands/PurePursuitCommand.cpp
+++ src/commands/PurePursuitCommand.cpp
@@ -1,111 +1,146 @@
 #include "PurePursuitCommand.h"
-#include <algorithm>
-#include <cmath>
 
 #include "../utils/transform.h"
 
-using namespace navtypes;
+#include <algorithm>
+#include <cmath>
 
-namespace commands {
-
-PurePursuitCommand::PurePursuitCommand(const points_t& waypoints, double driveVel, double doneThresh)
-					: _pose(pose_t::Zero()), _drive_vel(driveVel),
-					  _done_thresh(doneThresh), _set_state_called_before_output(false) {
-    interpolatePoints(waypoints);
-}
-
-
-command_t PurePursuitCommand::getOutput() {
-    point_t relIntersect = util::toTransform(_pose) * lineToCircleIntersection();
-    double radius = (relIntersect[0] * relIntersect[0] + relIntersect[1] * relIntersect[1]) /
-                    (2 * relIntersect[0] * relIntersect[1]);
-
-    double thetaVel = _drive_vel / radius; // curvature (1/r) times drive vel = theta vel
-
-    return { .thetaVel = thetaVel, .xVel = _drive_vel };
-}   
-
-point_t PurePursuitCommand::lineToCircleIntersection(point_t& p1, point_t& p2) {
-    // refer to https://mathworld.wolfram.com/Circle-LineIntersection.html
-    double currX = _pose(0);
-    double currY = _pose(1);
-    double x1 = p1[0] - _pose(0);
-    double y1 = p1[1] - _pose(1);
-    double x2 = p2[0] - _pose(0);
-    double y2 = p2[1] - _pose(1);
-
-    double dx = x2 - x1;
-    double dy = y2 - y1;
-    double dr = sqrt(pow(dx, 2) + pow(dy, 2));
-
-    double D = x1 * y2 - x2 * y1;
-
-    // If discriminant is 0, there is 1 intersections
-    //                 is > 0, there are 2 intersections
-    //                 is < 0, there are no intersections
-    double disc = pow(_lookahead_dist, 2) * pow(dr, 2) - pow(D, 2);
-
-    if (disc >= 0) {
-        double xSol1 = (D * dy + sgn(dy) * dx * sqrt(disc)) / (dr * dr);
-        double xSol2 = (D * dy - sgn(dy) * dx * sqrt(disc)) / (dr * dr);
-        double ySol1 = (-D * dx + abs(dy) * dx * sqrt(disc)) / (dr * dr);
-        double ySol2 = (-D * dx - abs(dy) * dx * sqrt(disc)) / (dr * dr);
-
-        point_t sol1 = {xSol1 + currX, ySol1 + currY};
-        point_t sol2 = {xSol2 + currX, ySol2 + currY};
-
-        double wMag1 = sqrt((x1 - xSol1) * (x1 - xSol1) + (y1 - ySol1) * (y1 - ySol1));
-        double wMag2 = sqrt((x2 - xSol1) * (x2 - xSol1) + (y2 - ySol1) * (y2 - ySol1);
-
-        // Let w be the vector from p1 to sol1. The proximity of sol1 to p1
-        // can be found simply through the magnitude of w since sol1 and p1
-        // are on the same line. We can scale the proximity score with the 
-        // distance from p1 to p2, such that the score at p1 is 0 and the 
-        // score at p2 is 1. The proximity score of a viable solution must 
-        // lie in [0, 1] to be considered as a valid solution. 
-        // The same applies to sol2.
-        double proxScore1 = wMag1 / dr;
-        double proxScore2 = wMag2 / dr;
-
-        if ((0 <= proxScore1 && proxScore1 <= 1) || (0 <= proxScore2 && proxScore2 <= 1)) {
-            if (proxScore2 < 0 || proxScore2 > 1) {
-                return sol1;
-            } else if (proxScore1 < 0 || proxScore1 > 1) {
-                return sol2;
-            } else {
-                return proxScore2 > proxScore1 ? sol2 : sol1;
-            }
-        } else {
-            // If no valid intersection is found, treat p2 as the intersection point.
-            return p2;
-        }
-    }
-}
-
-void PurePursuitCommand::interpolatePoints(const points_t& waypoints) {
-    if (waypoints.empty()) {
-        return;
-    }
-
-    _path.push_back();
-}
-
-} // namespace commands
+using navtypes::point_t;
+using navtypes::points_t;
+using navtypes::pose_t;
 
 namespace {
 static double wrapAngle(double angErr) {
-    return std::atan2(std::sin(angErr), std::cos(angErr));
+	return std::atan2(std::sin(angErr), std::cos(angErr));
 }
 
 static double sgn(double num) {
-    if (num < 0) {
-        return -1.0;
-    } else {
-        return 1.0;
-    }
+	if (num < 0) {
+		return -1.0;
+	} else {
+		return 1.0;
+	}
 }
 
-static double distSquared(point_t& p1, point_t& p2) {
-    return (p2[0] - p1[0]) * (p2[0] - p1[0]) + (p2[1] - p1[1]) * (p2[1] - p1[1]);
+static double dist(const point_t& p1, const point_t& p2) {
+	return sqrt((p2[0] - p1[0]) * (p2[0] - p1[0]) + (p2[1] - p1[1]) * (p2[1] - p1[1]));
 }
+} // namespace
+
+namespace commands {
+
+PurePursuitCommand::PurePursuitCommand(const points_t& waypoints, double driveVel,
+									   double doneThresh)
+	: _pose(pose_t::Zero()), _drive_vel(driveVel), _done_thresh(doneThresh),
+	  _set_state_called_before_output(false) {
+	interpolatePoints(waypoints);
 }
+
+command_t PurePursuitCommand::getOutput() {
+	point_t relIntersect;
+	if (_curr_idx >= _path.size() - 1) {
+		relIntersect = _path.back();
+	} else {
+		relIntersect = util::toTransform(_pose) *
+					   lineToCircleIntersection(_path[_curr_idx], _path[_curr_idx + 1]);
+	}
+	double radius = (relIntersect[0] * relIntersect[0] + relIntersect[1] * relIntersect[1]) /
+					(2 * relIntersect[0] * relIntersect[1]);
+
+	double thetaVel = _drive_vel / radius; // curvature (1/r) times drive vel = theta vel
+
+	return {.thetaVel = thetaVel, .xVel = _drive_vel};
+}
+
+point_t PurePursuitCommand::lineToCircleIntersection(point_t& p1, point_t& p2) {
+	// refer to https://mathworld.wolfram.com/Circle-LineIntersection.html
+
+	double currX = _pose(0);
+	double currY = _pose(1);
+	double x1 = p1[0] - _pose(0);
+	double y1 = p1[1] - _pose(1);
+	double x2 = p2[0] - _pose(0);
+	double y2 = p2[1] - _pose(1);
+
+	double dx = x2 - x1;
+	double dy = y2 - y1;
+	double dr = sqrt(pow(dx, 2) + pow(dy, 2));
+
+	double D = x1 * y2 - x2 * y1;
+
+	// If discriminant is 0, there is 1 intersections
+	//                 is > 0, there are 2 intersections
+	//                 is < 0, there are no intersections
+	double disc = pow(_lookahead_dist, 2) * pow(dr, 2) - pow(D, 2);
+
+	if (disc >= 0) {
+		double xSol1 = (D * dy + sgn(dy) * dx * sqrt(disc)) / (dr * dr);
+		double xSol2 = (D * dy - sgn(dy) * dx * sqrt(disc)) / (dr * dr);
+		double ySol1 = (-D * dx + abs(dy) * dx * sqrt(disc)) / (dr * dr);
+		double ySol2 = (-D * dx - abs(dy) * dx * sqrt(disc)) / (dr * dr);
+
+		point_t sol1 = {xSol1 + currX, ySol1 + currY};
+		point_t sol2 = {xSol2 + currX, ySol2 + currY};
+
+		double wMag1 = sqrt((x1 - xSol1) * (x1 - xSol1) + (y1 - ySol1) * (y1 - ySol1));
+		double wMag2 = sqrt((x2 - xSol1) * (x2 - xSol1) + (y2 - ySol1) * (y2 - ySol1));
+
+		// Let w be the vector from p1 to sol1. The proximity of sol1 to p1
+		// can be found simply through the magnitude of w since sol1 and p1
+		// are on the same line. We can scale the proximity score with the
+		// distance from p1 to p2, such that the score at p1 is 0 and the
+		// score at p2 is 1. The proximity score of a viable solution must
+		// lie in [0, 1] to be considered as a valid solution.
+		// The same applies to sol2.
+		double proxScore1 = wMag1 / dr;
+		double proxScore2 = wMag2 / dr;
+
+		if ((0 <= proxScore1 && proxScore1 <= 1) || (0 <= proxScore2 && proxScore2 <= 1)) {
+			if (proxScore2 < 0 || proxScore2 > 1) {
+				if (proxScore2 > 1) {
+					_curr_idx++;
+				}
+				return sol1;
+			} else if (proxScore1 < 0 || proxScore1 > 1) {
+				return sol2;
+			} else {
+				if (proxScore2 > proxScore1) {
+					_curr_idx++;
+					return sol2;
+				}
+				return sol1;
+			}
+		}
+	} else {
+		return p2;
+	}
+}
+
+void PurePursuitCommand::interpolatePoints(const points_t& waypoints) {
+	if (waypoints.empty()) {
+		return;
+	}
+	_path.push_back(waypoints[0]);
+	point_t lastPathPoint = waypoints[0];
+	int numPts = 1;
+	double accumulatedDist = 0;
+
+	for (int i = 0; i < waypoints.size() - 1; i++) {
+		point_t p1 = waypoints[i];
+		point_t p2 = waypoints[i + 1];
+		double segmentLen = dist(p1, p2);
+
+		while (accumulatedDist + segmentLen >= numPts * _dist_between_points) {
+			double t = (numPts * _dist_between_points - accumulatedDist) / segmentLen;
+			point_t newPt = {p1[0] + t * (p2[0] - p1[0]), p1[1] + t * (p2[1] - p1[1])};
+			_path.push_back(newPt);
+			lastPathPoint = newPt;
+			numPts++;
+		}
+
+		accumulatedDist += segmentLen;
+	}
+	_path.push_back(waypoints.back());
+}
+
+} // namespace commands
--- src/commands/PurePursuitCommand.h
+++ src/commands/PurePursuitCommand.h
@@ -1,52 +1,47 @@
 #pragma once
 
-#include "CommandBase.h"
 #include "../navtypes.h"
+#include "CommandBase.h"
 
 namespace commands {
 class PurePursuitCommand : CommandBase {
 public:
-    // 
-    PurePursuitCommand(const navtypes::points_t& waypoints, double driveVel, double doneThresh);
+	//
+	PurePursuitCommand(const navtypes::points_t& waypoints, double driveVel,
+					   double doneThresh);
 
-
-    command_t getOutput();
-    
+	command_t getOutput();
 
 private:
-    /** 
-     * @see c
-     */
-    point_t lineToCircleIntersection(navtypes::point_t& p1, navtypes::point_t& p2);
+	/**
+	 * @see c
+	 */
+	navtypes::point_t lineToCircleIntersection(navtypes::point_t& p1, navtypes::point_t& p2);
 
-    void interpolatePoints(const points_t& waypoints);
+	void interpolatePoints(const points_t& waypoints);
 
-    /**
-     * Wrap angle error to [-pi, pi] to prevent 90+ degree turns
-     */
-    double wrapAngle(double angErr);
+	/**
+	 * Wrap angle error to [-pi, pi] to prevent 90+ degree turns
+	 */
+	double wrapAngle(double angErr);
 
-    /**
-     * Helper function to determine "sign" of a number. Treats 0 as positive.
-     * 
-     * @returns 1 if num >= 0, -1 if num < 0
-     * 
-     * @see https://mathworld.wolfram.com/Circle-LineIntersection.html
-     */
-    double sgn(double num);
+	/**
+	 * Helper function to determine "sign" of a number. Treats 0 as positive.
+	 *
+	 * @returns 1 if num >= 0, -1 if num < 0
+	 *
+	 * @see https://mathworld.wolfram.com/Circle-LineIntersection.html
+	 */
+	double sgn(double num);
 
-    // transform robo frame
+	int _curr_idx;
+	pose_t _pose;
+	double _drive_vel;
+	double _done_thresh;
+	double _dist_between_points;
+	double _lookahead_dist;
 
-    // find curvature
-
-
-    pose_t _pose;
-    double _drive_vel;
-    double _done_thresh;
-    double _lookahead_dist;
-    bool _set_state_called_before_output;
-    navtypes:: points_t _path;
-    
-    
+	bool _set_state_called_before_output;
+	navtypes::points_t _path;
 };
 } // namespace commands
